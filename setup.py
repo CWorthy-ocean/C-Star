e3fb3ae8a81d460978c827a1a504f8ef8364cf5b
“Dafydd <dafydd_stephenson@hotmail.com>
2024-05-07 11:36:16 -0600

    First complete local conda install: - Overhaul setup.py to import manic from manage_externals and checkout ROMS and MARBL - Write a build.sh to determine system on which build is taking place and compile MARBL, NHMG, and Tools-Roms - Modify Externals.cfg to use new flexible_makefile ROMS branch to allow configurability of ROMS compile Currently, conda will only fetch dependencies (GNU) on macOS ARM64. If user is on a supported HPC cluster, Linux Environment Modules will be used to get dependencies (intel). Future version should include the ability to default back to GNU if on an unsupported system, which currently just throws an error.

--- setup.py
+++ setup.py
@@ -1,10 +1,39 @@
-from setuptools import setup
+from setuptools import setup,find_packages
+from setuptools.command.install import install
+import os,sys
 
 ################################################################################
 
+class install_with_externals(install):
+    '''
+    Customise the setuptools default install procedure
+    '''
+    def run(self):
+        install.run(self) # run the default install procedure first
+
+        self.checkout() # run our checkout externals function
+        
+    def checkout(self):
+        # Add manage_externals to the path so we can import manic
+        parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'manage_externals'))
+        sys.path.insert(0, parent_dir)
+        
+        import manic
+        
+        # Unclear how to call manic.checkout without CLI so just passing redundant args:
+        ARGS=manic.checkout.commandline_arguments(['--externals','Externals.cfg'])
+        
+        try:
+            manic.checkout.main(ARGS)
+        except Exception as error:  # pylint: disable=broad-except
+            manic.printlog(str(error))
+            if ARGS.backtrace:
+                traceback.print_exc()
+
+
 setup(
     name = 'cstar_ocean',
-    packages = ['cstar_ocean'],
+    packages = find_packages(),#['cstar_ocean'],
     version = '0.0.0',
     description = 'Computation Systems for Tracking Ocean Carbon (CSTAR) python package',
     author = 'Dafydd Stephenson, Thomas Nicholas, and others',
@@ -15,10 +44,14 @@ setup(
     #data_files=[()],
     python_requires = '>=3.10',
     #install_requires = dependency_list,
-    #tests_require=[python packages needed for testing],
+    #setup_requires=['pytest-runner'],
+    #tests_require=['pytest'],
     license='MIT',
     #classifiers=[list of even more metadata],
     #long_description=<print contents of a README here>,
     #long_description_content_type='text/markdown',
+    cmdclass={
+        'install': install_with_externals,
+    },
 )
     
