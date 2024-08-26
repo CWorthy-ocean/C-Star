611c8f3346bd7b0d0c2f9bbb8187905effa3f41f
TomNicholas <tom@cworthy.org>
2024-08-26 15:16:14 -0400

    sketch out pytest version of roms_marbl test

--- tests/test_roms_marbl_example.py
+++ tests/test_roms_marbl_example.py
@@ -1,64 +1,62 @@
-import os
-import shutil
-import subprocess
-from unittest.mock import patch
+from pathlib import Path
+import yaml
+
 import cstar
+import pytest
 
-## First test uses URLs to point to input datasets
-roms_marbl_remote_case = cstar.Case.from_blueprint(
-    blueprint=(cstar.base.environment._CSTAR_ROOT)
-    + "/../examples/cstar_blueprint_roms_marbl_example.yaml",
-    caseroot="roms_marbl_example_case/",
-    start_date="20120103 12:00:00",
-    end_date="20120103 12:30:00",
-)
 
-# patch will automatically respond "y" to any call for input
-with patch("builtins.input", return_value="y"):
-    roms_marbl_remote_case.setup()
-    roms_marbl_remote_case.persist("test_blueprint.yaml")
-    roms_marbl_remote_case.build()
-    roms_marbl_remote_case.pre_run()
-    roms_marbl_remote_case.run()
-    roms_marbl_remote_case.post_run()
+ROMS_MARBL_BASE_BLUEPRINT_PATH = 'test_blueprint.yaml'
 
-print("Test complete with remote input dataset files")
 
-## Second test modifies the yaml created above to use available local input datasets
-
-# Move the input datasets to a new location
-shutil.move(roms_marbl_remote_case.caseroot+'/input_datasets/ROMS/','local_input_files')
-
-# Modify the blueprint file to point to local paths whenever we have the files:
-with open('test_blueprint.yaml') as f:
-    test_blueprint=f.readlines()
-
-for i,line in enumerate(test_blueprint):
-    url_prefix="https://github.com/CWorthy-ocean/input_datasets_roms_marbl_example/raw/main/"
-    if url_prefix in line:
-        fileurl=line.split()[-1] # Just isolate URL from e.g. source: URL
-        filepath=f"{os.getcwd()}/local_input_files/{os.path.basename(fileurl)}"
-        if os.path.exists(filepath):
-            test_blueprint[i]=line.replace(fileurl,filepath)
-
-with open('modified_test_blueprint.yaml', 'w') as f:
-    f.writelines(test_blueprint)
-##
-
-roms_marbl_local_case = cstar.Case.from_blueprint(
-    blueprint="modified_test_blueprint.yaml",
-    caseroot="roms_marbl_local_case",
-    start_date="20120103 12:00:00",
-    end_date="20120103 12:30:00")
-
-# patch will automatically respond "y" to any call for input
-with patch("builtins.input", return_value="y"):
-    roms_marbl_local_case.setup()
-    roms_marbl_local_case.persist("test_blueprint_local.yaml")
-    roms_marbl_local_case.build()
-    roms_marbl_local_case.pre_run()
-    roms_marbl_local_case.run()
-    roms_marbl_local_case.post_run()
-
-print("Test complete with local input dataset files")
+@pytest.fixture
+def roms_marbl_blueprint_remote_datasets(tmp_path) -> Path:
+    """Creates blueprint yaml file in temporary directory"""
     
+    with open(ROMS_MARBL_BASE_BLUEPRINT_PATH, 'r') as file:
+        blueprint_dict = yaml.load(file, Loader=yaml.Loader)
+        
+    blueprint_filepath = tmp_path / 'blueprint.yaml'
+
+    with open(blueprint_filepath, 'w') as file:
+        yaml.dump(blueprint_dict, file)
+
+    return blueprint_filepath
+
+
+# TODO similar fixture but the blueprint file contains paths to local files instead
+# TODO should these local files be in a given temporary directory?
+
+
+class TestRomsMarbl:
+    def test_roms_marbl_remote_files(self, tmpdir, monkeypatch, roms_marbl_blueprint_remote_datasets):
+        """Test using URLs to point to input datasets"""
+
+        roms_marbl_remote_case = cstar.Case.from_blueprint(
+            blueprint=roms_marbl_blueprint_remote_datasets,
+            caseroot=tmpdir,
+            start_date="20120103 12:00:00",
+            end_date="20120103 12:30:00",
+        )
+
+        # monkeypatch will automatically respond "y" to any call for input
+        with monkeypatch.context() as m:
+            m.setattr("builtins.input", "y")
+            
+            # do we actually need user input for all these steps?
+            roms_marbl_remote_case.setup()
+
+            # why are we persisting this blueprint file then not using it again in the test?
+            roms_marbl_remote_case.persist(tmpdir / "test_blueprint.yaml")
+
+            roms_marbl_remote_case.build()
+            roms_marbl_remote_case.pre_run()
+            roms_marbl_remote_case.run()
+            roms_marbl_remote_case.post_run()
+
+    @pytest.mark.skip(reason='not yet implemented')
+    def test_roms_marbl_local_files(self, tmpdir, roms_marbl_blueprint_local_datasets):
+        """Test using available local input datasets"""
+
+        # TODO have a fixture that downloads the files to a temporary directory
+        # Does that basically just mean running case.setup()?
+        ...
