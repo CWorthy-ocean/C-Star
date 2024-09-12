d425021640e7e96dad305ab77e8a09cdaa8b8bb6
TomNicholas <tom@cworthy.org>
2024-09-12 17:10:04 -0400

    link to issue

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ import pytest
 
 
 # Prevent errors caused by pytest trying to collect tests from externals 
-# (e.g. from errors whilst importing python code in the UCLA-roms repo)
+# (e.g. from errors whilst importing python code in the UCLA-roms repo, see https://github.com/CWorthy-ocean/C-Star/issues/53)
 collect_ignore_glob = [
     "cstar/cstar_local_config.py",
     "cstar/externals/**"
