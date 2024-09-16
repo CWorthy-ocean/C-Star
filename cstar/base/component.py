d9d0ccc7d9355e59c27ca10f2e9532523eff2f9f
Dafydd S <102196046+dafyddstephenson@users.noreply.github.com>
2024-09-16 14:23:10 -0600

    Discretization class (#71)
    * add discretization class; some related tidying of Case.persist, Case.from_blueprint; no longer use kwargs: Any to initialise Component
    
    * overhaul Case.from_blueprint to be cleaner, passing the dictionary read from yaml straight to the constructor method for each class and not repeating code for each kind of InputDataset
    
    * tidy up and update docstrings
    
    * update example notebook
    
    * fix bug where Component.discretization being None led to problems with Case.persist
    
    * linting
    
    * fix mutable default type for input_datasets
    
    * Tweaks following PR review:
    - Split instances of multiple similar variables on one line in docstrings
    - Remove nx,ny, levels attrs from Discretization class
    - Reinstate type check for BaseModel in Component.__init__()
    
    * update blueprint to reflect changes
    
    * update example notebook
    
    * Update cstar/base/component.py
    
    Co-authored-by: Tom Nicholas <tom@cworthy.org>
    
    * Update cstar/base/component.py
    
    Co-authored-by: Tom Nicholas <tom@cworthy.org>
    
    * remove assert from component.py
    
    ---------
    
    Co-authored-by: “Dafydd <“dafydd_stephenson@hotmail.com”>
    Co-authored-by: TomNicholas <tom@cworthy.org>
    Co-authored-by: Nora Loose <NoraLoose@users.noreply.github.com>

--- cstar/base/component.py
+++ cstar/base/component.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import List, Optional, Any, TYPE_CHECKING
+from typing import Optional, TYPE_CHECKING
 
 from cstar.base.base_model import BaseModel
 from cstar.base.input_dataset import InputDataset
@@ -24,6 +24,9 @@ class Component(ABC):
     input_datasets: list of InputDatasets
         Any spatiotemporal data needed to run this instance of the base model
         e.g. initial conditions, surface forcing, etc.
+    discretization: Discretization
+        Any information related to the discretization of this Component
+        e.g. time step, number of vertical levels, etc.
 
     Methods:
     -------
@@ -37,7 +40,13 @@ class Component(ABC):
         Execute any post-processing actions associated with this component
     """
 
-    def __init__(self, **kwargs: Any):
+    def __init__(
+        self,
+        base_model: BaseModel,
+        additional_code: Optional["AdditionalCode"],
+        input_datasets: list[InputDataset] | None = None,
+        discretization: Optional["Discretization"] = None,
+    ):
         """
         Initialize a Component object from a base model and any additional_code or input_datasets
 
@@ -46,32 +55,29 @@ class Component(ABC):
         base_model: BaseModel
             An object pointing to the unmodified source code of a model handling an individual
             aspect of the simulation such as biogeochemistry or ocean circulation
-        additional_code: AdditionalCode or list of AdditionalCodes
+        additional_code: AdditionalCode (Optional, default None)
             Additional code contributing to a unique instance of a base model,
             e.g. namelists, source modifications, etc.
-        input_datasets: list of InputDatasets
+        input_datasets: list of InputDatasets (Optional, default [])
             Any spatiotemporal data needed to run this instance of the base model
             e.g. initial conditions, surface forcing, etc.
+        discretization: Discretization (Optional, default None)
+            Any information related to the discretization of this Component (e.g. time step)
+
 
         Returns:
         --------
         Component:
             An intialized Component object
         """
-
-        # TODO: do Type checking here
-        if "base_model" not in kwargs or not isinstance(
-            kwargs["base_model"], BaseModel
-        ):
+        if not isinstance(base_model, BaseModel):
             raise ValueError(
                 "base_model must be provided and must be an instance of BaseModel"
             )
-        self.base_model: BaseModel = kwargs["base_model"]
-
-        self.additional_code: Optional["AdditionalCode"] = kwargs.get(
-            "additional_code", None
-        )
-        self.input_datasets: List[InputDataset] = kwargs.get("input_datasets") or []
+        self.base_model = base_model
+        self.additional_code = additional_code or None
+        self.input_datasets = [] if input_datasets is None else input_datasets
+        self.discretization = discretization or None
 
     def __str__(self) -> str:
         # Header
@@ -87,39 +93,16 @@ class Component(ABC):
 
         NID = len(self.input_datasets)
 
-        base_str += f"\n{NAC} AdditionalCode repositories (query using Component.additional_code)"
         base_str += (
-            f"\n{NID} InputDataset objects (query using Component.input_datasets"
+            f"\n{NAC} AdditionalCode instances (query using Component.additional_code)"
         )
-
-        # Discretisation
-        disc_str = ""
-        if hasattr(self, "time_step") and self.time_step is not None:
-            disc_str += "\ntime_step: " + str(self.time_step) + "s"
-        if hasattr(self, "n_procs_x") and self.n_procs_x is not None:
-            disc_str += (
-                "\nn_procs_x: "
-                + str(self.n_procs_x)
-                + " (Number of x-direction processors)"
-            )
-        if hasattr(self, "n_procs_y") and self.n_procs_y is not None:
-            disc_str += (
-                "\nn_procs_y:"
-                + str(self.n_procs_y)
-                + " (Number of y-direction processors)"
-            )
-        if hasattr(self, "n_levels") and self.n_levels is not None:
-            disc_str += "\nn_levels:" + str(self.n_levels)
-        if hasattr(self, "nx") and self.nx is not None:
-            disc_str += "\nnx:" + str(self.nx)
-        if hasattr(self, "ny") and self.ny is not None:
-            disc_str += "\nny:" + str(self.ny)
+        base_str += f"\n{NID} Input datasets (query using Component.input_datasets)"
+        if hasattr(self, "discretization") and self.discretization is not None:
+            base_str += "\n\nDiscretization:\n"
+            base_str += self.discretization.__str__()
         if hasattr(self, "exe_path") and self.exe_path is not None:
-            disc_str += "\n\nIs compiled: True"
-            disc_str += "\n exe_path: " + self.exe_path
-        if len(disc_str) > 0:
-            disc_str = "\n\nDiscretization info:" + disc_str
-        base_str += disc_str
+            base_str += "\n\nIs compiled: True"
+            base_str += "\n exe_path: " + self.exe_path
         return base_str
 
     def __repr__(self) -> str:
@@ -158,3 +141,49 @@ class Component(ABC):
         This abstract method will be implemented differently by different Component types.
         """
         pass
+
+
+class Discretization(ABC):
+    """
+    Holds discretization information about a Component.
+
+    Attributes:
+    -----------
+
+    time_step: int
+        The time step with which to run the Component
+    """
+
+    def __init__(
+        self,
+        time_step: int,
+    ):
+        """
+        Initialize a Discretization object from basic discretization parameters
+
+        Parameters:
+        -----------
+        time_step: int
+            The time step with which to run the Component
+
+        Returns:
+        --------
+        Discretization:
+            An initialized Discretization object
+
+        """
+
+        self.time_step: int = time_step
+
+    def __str__(self) -> str:
+        # Discretisation
+        disc_str = ""
+
+        if hasattr(self, "time_step") and self.time_step is not None:
+            disc_str += "\ntime_step: " + str(self.time_step) + "s"
+        if len(disc_str) > 0:
+            classname = self.__class__.__name__
+            header = classname
+            disc_str = header + "\n" + "-" * len(classname) + disc_str
+
+        return disc_str
