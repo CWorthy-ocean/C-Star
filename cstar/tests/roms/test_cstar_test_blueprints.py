d63297b02cc6eb688f2f0fc78cfc7a12266eb774
“Dafydd <dafydd_stephenson@hotmail.com>
2024-09-27 17:23:32 -0600

    add docstring for primary c-star test

--- cstar/tests/roms/test_cstar_test_blueprints.py
+++ cstar/tests/roms/test_cstar_test_blueprints.py
@@ -22,6 +22,34 @@ class TestCStar:
         fetch_remote_test_case_data,
         test_config_key,
     ):
+        """
+        Run the C-Star minimal test case from a selection of different blueprints.
+
+        Parameters:
+        -----------
+        tmpdir:
+           Built-in pytest fixture to create temporary directories
+        mock_user_input:
+           Fixture to simulate user-supplied input
+        modify_template_blueprint:
+           Fixture to modify the contents of a template blueprint and save to a
+           temporary file (from which the Case instance is created)
+        fetch_roms_tools_source_data:
+           Fixture to fetch source data needed by roms-tools to generate input
+           datasets for ROMS. Source data are saved to the system cache at the
+           location specified by ROMS_TOOLS_DATA_DIRECTORY in the config.py module.
+           A symlink to this cache directory containing this source data is
+           created wherever this test is run and deleted on exit.
+        fetch_remote_test_case_data:
+           Fixture to fetch data needed for the C-Star test case, such as additional
+           code for compiling ROMS, yaml files to supply to roms-tools, etc.
+           These are saved to a subdirectory of the system cache specified by
+           CSTAR_TEST_DATA_DIRECTORY in the config.py module
+        test_config_key (str):
+           Key determining the specific variation of the test case that is run, as
+           defined in the dictionary TEST_CONFIG in the config.py module
+        """
+
         # Regardless of remote or local, if yaml_datasets we need roms-tools support data
         if "yaml_datasets" in test_config_key:
             fetch_roms_tools_source_data(symlink_path="roms_tools_datasets")
