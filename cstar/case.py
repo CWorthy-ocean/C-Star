a13710d7515a49f34ba3743711ce4bf3612dc754
Dafydd <dafydd_stephenson@hotmail.com>
2025-01-23 11:52:12 -0800

    fix typo in doc 3; add safe assertion to stop mypy error; remove in-repo blueprints (now hosted elsewhere)

--- cstar/case.py
+++ cstar/case.py
@@ -704,8 +704,14 @@ class Case:
 
         new_case = copy.deepcopy(self)
         new_case.start_date = self.end_date
-        new_case.caseroot = new_case.caseroot / f"RESTART_{new_case.start_date.strftime(format='%Y%m%d_%H%M%S')}"
-        
+        assert isinstance(
+            new_case.start_date, datetime
+        ), "old Case.end_date must be datetime (see __init__), passing this forward"
+        new_case.caseroot = (
+            new_case.caseroot
+            / f"RESTART_{new_case.start_date.strftime(format='%Y%m%d_%H%M%S')}"
+        )
+
         if isinstance(new_end_date, str):
             new_case.end_date = dateutil.parser.parse(new_end_date)
         elif isinstance(new_end_date, datetime):
@@ -730,5 +736,5 @@ class Case:
             new_components.append(new_component)
         new_case.components = new_components
         print(f"Created new Case to restart case {self.name!r} in {new_case.caseroot}")
-        
+
         return new_case
