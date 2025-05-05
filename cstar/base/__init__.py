7dfa5a541909f7f817cd9071f6dc5f0b6e27c94a
Christopher McBride <3595025+ankona@users.noreply.github.com>
2025-05-05 15:29:51 -0400

    Mitigate lint warnings

--- cstar/base/__init__.py
+++ cstar/base/__init__.py
@@ -1,8 +1,8 @@
+from cstar.base import exceptions
 from cstar.base.additional_code import AdditionalCode
 from cstar.base.discretization import Discretization
 from cstar.base.external_codebase import ExternalCodeBase
 from cstar.base.input_dataset import InputDataset
-from cstar.base import exceptions
 
 __all__ = [
     "Component",
