437d293d0472d1349a23f738d8416457baa5d197
Christopher McBride <3595025+ankona@users.noreply.github.com>
2025-05-14 18:49:25 -0400

    Enable logging to a file with `get_logger`

---
+++ cstar/tests/unit_tests/base/test_log.py
@@ -0,0 +1,132 @@
+import logging
+import logging.handlers
+import pathlib
+import typing as t
+import uuid
+
+import pytest
+
+from cstar.base.log import get_logger
+
+
+@pytest.fixture
+def all_levels() -> list[int]:
+    return [
+        logging.DEBUG,
+        logging.INFO,
+        logging.WARNING,
+        logging.ERROR,
+        logging.CRITICAL,
+    ]
+
+
+@pytest.fixture
+def levels_fn(
+    all_levels: list[int],
+) -> t.Callable[[int], t.Tuple[list[int], list[int]]]:
+    """Return a function that returns a tuple of lists containing all log levels below
+    and above the specified log level.
+
+    Returns:
+        t.Callable[list[int], list[int]]: the function
+    """
+
+    def _inner(level: int) -> t.Tuple[list[int], list[int]]:
+        """Return a tuple containing:
+        1. list of levels less than the supplied level
+        2. list of levels greater than or equal to the supplied level
+
+        Args:
+            level (int): the target log level
+
+        Returns:
+            tuple[list[int], list[int]]: the lists of log levels
+        """
+        lt, gte = [], []
+
+        for level_ in all_levels:
+            if level_ < level:
+                lt.append(level_)
+            elif level_ >= level:
+                gte.append(level_)
+
+        return lt, gte
+
+    return _inner
+
+
+@pytest.mark.parametrize(
+    "level",
+    [
+        logging.DEBUG,
+        logging.INFO,
+        logging.WARNING,
+        logging.ERROR,
+        logging.CRITICAL,
+    ],
+)
+def test_loglevel_fh(
+    request: pytest.FixtureRequest,
+    level: int,
+    levels_fn: t.Callable[[int], tuple[list[int], list[int]]],
+    all_levels: list[int],
+    tmp_path: pathlib.Path,
+) -> None:
+    """Verify the loggers are configured properly to output the desired log levels when
+    the optional file handler is requested."""
+
+    for level in all_levels:
+        logger_name = f"{request.function.__name__}-{level}"
+        filename = tmp_path / f"{logger_name}.log"
+
+        log = get_logger(logger_name, level, filename=str(filename))
+
+        lt_levels, gt_eq_levels = levels_fn(level)
+
+        msg = str(uuid.uuid4())
+        funcs = [log.debug, log.info, log.warning, log.error, log.critical]
+
+        for msg_level, log_fn in zip(all_levels, funcs):
+            log_fn(msg)
+            log_content = filename.read_text()
+
+            # confirm all messages < level aren't in file
+            if msg_level in list(gt_eq_levels):
+                expected_log_entry = f"[{logging._levelToName[msg_level]}] {msg}"
+                assert expected_log_entry in log_content
+
+            # confirm messages >= level are in file
+            if msg_level in list(lt_levels):
+                assert msg not in log_content
+
+
+def test_filehandler_no_dupes(
+    request: pytest.FixtureRequest,
+    tmp_path: pathlib.Path,
+) -> None:
+    """Verify the loggers are configured properly to output the desired log levels when
+    the optional file handler is requested."""
+
+    level = logging.INFO
+    logger_name = f"{request.function.__name__}-{level}"
+    filename = tmp_path / f"{logger_name}.log"
+
+    # ask for log repeatedly, fh should only be added once.
+    log = get_logger(logger_name, level, filename=filename)
+    log = get_logger(logger_name, level, filename=filename)
+    log = get_logger(logger_name, level, filename=filename)
+    log = get_logger(logger_name, level, filename=filename)
+
+    msg = str(uuid.uuid4())
+    log.info(msg)
+
+    log_content = filename.read_text()
+    expected_log_entry = f"[{logging._levelToName[level]}] {msg}"
+
+    # confirm entry is found...
+    found_at = log_content.find(expected_log_entry)
+    assert found_at > -1
+
+    # ... and isn't written more than once
+    found_at = log_content.find(expected_log_entry, len(expected_log_entry))
+    assert found_at == -1
