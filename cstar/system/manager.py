8483c49e4f71a6bab602aaca29f56786d5133b10
Christopher McBride <3595025+ankona@users.noreply.github.com>
2025-05-21 18:47:13 -0400

    tweaks

--- cstar/system/manager.py
+++ cstar/system/manager.py
@@ -39,7 +39,7 @@ class SystemName(Enum):
     DERECHO = "derecho"
     DARWIN_ARM64 = "darwin_arm64"
     LINUX_X86_64 = "linux_x86_64"
-    LINUX_ARM64 = "linux_aarch64"
+    LINUX_AARCH64 = "linux_aarch64"
 
 
 class CStarSystemManager:
@@ -170,7 +170,7 @@ class CStarSystemManager:
             case (
                 SystemName.DARWIN_ARM64
                 | SystemName.LINUX_X86_64
-                | SystemName.LINUX_ARM64
+                | SystemName.LINUX_AARCH64
             ):
                 mpi_exec_prefix = "mpirun"
                 compiler = "gnu"
