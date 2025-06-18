774a03fa2cc42bb3aecbf6c969e4c48150dca9ac
Chris McBride <3595025+ankona@users.noreply.github.com>
2025-06-18 06:23:58 -0400

    fix unnecessary conditional (#274)

--- cstar/simulation.py
+++ cstar/simulation.py
@@ -335,9 +335,10 @@ class Simulation(ABC, LoggingMixin):
             NN = len(self.compile_time_code.files)
             base_str += f"Compile-time code: {self.compile_time_code.__class__.__name__} instance with {NN} files (query using {class_name}.compile_time_code)"
 
-        if hasattr(self, "exe_path") and self.exe_path is not None:
+        exe_path = getattr(self, "exe_path", None)
+        if exe_path is not None:
             base_str += "\nIs compiled: True"
-            base_str += "\nExecutable path: " + str(self.exe_path)
+            base_str += "\nExecutable path: " + str(exe_path)
 
         return base_str
 
