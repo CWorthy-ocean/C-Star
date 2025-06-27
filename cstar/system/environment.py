c735b4851de523f3e0bc8ce83d195211f7939060
Chris McBride <3595025+ankona@users.noreply.github.com>
2025-06-27 11:55:21 -0400

    Improved CStarEnvironment loading behavior (#286)
    * Avoids unnecessary reloading of environment variables
    * Exposes system_env_path property

--- cstar/system/environment.py
+++ cstar/system/environment.py
@@ -3,7 +3,7 @@ import os
 import platform
 from pathlib import Path
 
-from dotenv import dotenv_values, load_dotenv, set_key
+from dotenv import dotenv_values, set_key
 
 from cstar.base.utils import _run_cmd
 
@@ -59,11 +59,23 @@ class CStarEnvironment:
         mpi_exec_prefix: str,
         compiler: str,
     ):
-        # Initialize private attributes
+        """Initialize the instance.
+
+        Parameters
+        ----------
+        system_name : str
+            The name of the hosting platform
+        mpi_exec_prefix : str
+            The MPI prefix
+        compiler : str
+            The compiler to use for builds
+        """
         self._system_name = system_name
         self._mpi_exec_prefix = mpi_exec_prefix
         self._compiler = compiler
+        self._PACKAGE_ROOT: Path = self._find_package_root()
         self._CSTAR_USER_ENV_PATH = Path("~/.cstar.env").expanduser()
+        self._env_vars = self._load_env()
 
         if self.uses_lmod:
             self.load_lmod_modules(lmod_file=self.lmod_path)
@@ -114,21 +126,61 @@ class CStarEnvironment:
             ">"
         )
 
-    @property
-    def environment_variables(self) -> dict:
-        env_vars = dotenv_values(
-            self.package_root / f"additional_files/env_files/{self._system_name}.env"
-        )
+    def _load_env(self) -> dict[str, str]:
+        """Load environment variables from system and user .env files into memory.
+
+        Returns
+        -------
+        dict[str, str]
+            The variables that were loaded
+        """
+        env_vars = dotenv_values(self.system_env_path)
         user_env_vars = dotenv_values(self.user_env_path)
         env_vars.update(user_env_vars)
+
+        env_vars = {k: v for k, v in env_vars.items() if v is not None}
+        os.environ.update(env_vars)
+
         return env_vars
 
     @property
-    def package_root(self) -> Path:
-        """Identifies the root directory of the top-level package.
+    def environment_variables(self) -> dict[str, str]:
+        """Return the environment variables that were loaded from .env files.
+
+        Returns
+        -------
+        dict[str, str]
+            The key-value pairs that have been loaded.
+        """
+        return self._env_vars.copy()
+
+    @classmethod
+    def _find_package_root(cls) -> Path:
+        """Determine the root directory containing the source for this package.
 
         Uses `importlib.util.find_spec` to locate the package directory, enabling
         access to additional configuration files within the package structure.
+
+        Returns
+        -------
+        Path
+            The path to the source code directory.
+
+        Raises
+        ------
+        ImportError
+            When the package root directory cannot be identified.
+        """
+        top_level_package_name = __name__.split(".")[0]
+        spec = importlib.util.find_spec(top_level_package_name)
+        if spec is not None and isinstance(spec.submodule_search_locations, list):
+            return Path(spec.submodule_search_locations[0])
+
+        raise ImportError(f"Top-level package '{top_level_package_name}' not found.")
+
+    @property
+    def package_root(self) -> Path:
+        """Return the root directory of the top-level package.
 
         Returns
         -------
@@ -140,12 +192,7 @@ class CStarEnvironment:
         ImportError
             If the top-level package cannot be located.
         """
-        top_level_package_name = __name__.split(".")[0]
-        spec = importlib.util.find_spec(top_level_package_name)
-        if spec is not None:
-            if isinstance(spec.submodule_search_locations, list):
-                return Path(spec.submodule_search_locations[0])
-        raise ImportError(f"Top-level package '{top_level_package_name}' not found.")
+        return self._PACKAGE_ROOT
 
     # Environment management related
     @property
@@ -170,6 +217,18 @@ class CStarEnvironment:
             The path to the `.env` file.
         """
         return self._CSTAR_USER_ENV_PATH
+
+    @property
+    def system_env_path(self) -> Path:
+        """Identify the expected path to a .env file for the current system.
+
+        Returns
+        -------
+        Path
+            The path to the `.env` file.
+        """
+        pkg_relative_path = f"additional_files/env_files/{self._system_name}.env"
+        return self.package_root / pkg_relative_path
 
     @property
     def lmod_path(self) -> Path:
@@ -271,4 +330,4 @@ class CStarEnvironment:
             The value to set for the environment variable.
         """
         set_key(self.user_env_path, key, value)
-        load_dotenv(self.user_env_path, override=True)
+        self._env_vars = self._load_env()
