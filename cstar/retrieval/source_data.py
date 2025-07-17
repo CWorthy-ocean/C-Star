0248c8444ce0c71393e1fd17bcafe11d065acb01
Dafydd Stephenson <dafydd_stephenson@hotmail.com>
2025-07-17 16:44:10 -0600

    add module skeletons for retrieval/source_data.py, retrieval/retriever.py, and retrieval/retrieved_data.py

---
+++ cstar/retrieval/source_data.py
@@ -0,0 +1,85 @@
+from enum import Enum
+from pathlib import Path
+from typing import TYPE_CHECKING, Dict
+
+if TYPE_CHECKING:
+    from cstar.retrieval import RetrievedData, Retriever
+
+
+class SourceType(Enum):
+    FILE = "file"
+    DIRECTORY = "directory"
+    REPOSITORY = "repository"
+
+
+class LocationType(Enum):
+    URL = "url"
+    PATH = "path"
+
+
+class FileType(Enum):
+    YAML = "yaml"
+    NETCDF = "netcdf"
+
+
+class FileEncoding(Enum):
+    TEXT = "text"
+    BINARY = "binary"
+
+
+FILE_ENCODING_LOOKUP: Dict[FileType, FileEncoding] = {
+    FileType.YAML: FileEncoding.TEXT,
+    FileType.NETCDF: FileEncoding.BINARY,
+}
+
+
+class SourceData:
+    def __init__(
+        self, location: str | Path, file_hash: str | None, checkout_target: str | None
+    ):
+        self._location = str(location)
+        self._file_hash = file_hash
+        self._checkout_target = checkout_target
+        self._retriever = self._select_retriever()
+
+    # non-public attrs:
+    @property
+    def location(self) -> str:
+        return self._location
+
+    @property
+    def file_hash(self) -> str | None:
+        return self._file_hash
+
+    # Inferred data characteristics
+    @property
+    def location_type(self) -> LocationType:
+        """Determine type of location (path or URL) from 'location'."""
+        raise NotImplementedError
+
+    @property
+    def source_type(self) -> SourceType:
+        """Infer source type (file/directory/repository) from 'location'."""
+        raise NotImplementedError
+
+    @property
+    def file_type(self) -> FileType | None:
+        """Infer file type from 'location'."""
+        raise NotImplementedError
+
+    @property
+    def file_encoding(self) -> FileEncoding | None:
+        """Look up file encoding based on source_type."""
+
+        return (
+            FILE_ENCODING_LOOKUP.get(self.file_type, None) if self.file_type else None
+        )
+
+    # Retrieval logic
+    def _select_retriever(self) -> "Retriever":
+        """Logic to determine the correct retriever based on the above
+        characteristics."""
+        raise NotImplementedError
+
+    def get(self, target_path: str | Path) -> "RetrievedData":
+        return self._retriever.retrieve(target_path=Path(target_path), source=self)
