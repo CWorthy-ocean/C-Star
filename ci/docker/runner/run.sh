5e73893c1e6032a4c583f124252279a3034207a4
Christopher McBride <3595025+ankona@users.noreply.github.com>
2025-07-22 12:56:15 -0400

    fix deprecated module name

--- ci/docker/runner/run.sh
+++ ci/docker/runner/run.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-python -m cstar.scripts.runner.worker "$@"
-# python -m cstar.entrypoint.worker.worker \
-#     --log-level DEBUG \
-#     --blueprint-uri /opt/blueprint.yml \
-#     --output-dir /opt/workeroutput
+python -m cstar.entrypoint.worker.worker "$@"
