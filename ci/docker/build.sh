6a48b5be15eb83590692a0cb2b15e3906020f258
Christopher McBride <3595025+ankona@users.noreply.github.com>
2025-07-29 12:08:51 -0400

    remove --no-cache on builds

--- ci/docker/build.sh
+++ ci/docker/build.sh
@@ -101,10 +101,10 @@ chmod a+r $CONTEXT_PATH/entrypoint.sh
 
 if [ ! -z "$BUILD_ARGS" ]; then
     log_info "Building parameterized $VTAG in $CONTEXT_PATH from source \n$CONTEXT_DIR\n$BUILD_ARGS"
-    $RUNTIME_ENGINE build -t "$VTAG" --no-cache --build-arg "$BUILD_ARGS" "$CONTEXT_PATH" 
+    $RUNTIME_ENGINE build -t "$VTAG" --build-arg "$BUILD_ARGS" "$CONTEXT_PATH" 
 else
     log_info "Building $VTAG in $CONTEXT_PATH from source \n $CONTEXT_DIR"
-    $RUNTIME_ENGINE build -t "$VTAG" --no-cache "$CONTEXT_PATH"
+    $RUNTIME_ENGINE build -t "$VTAG" "$CONTEXT_PATH"
 fi
 
 log_info "Migrating $VTAG"
