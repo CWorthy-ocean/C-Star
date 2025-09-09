73262c7fe009cd203e5bf3d60b1ba359b34f9941
Christopher McBride <3595025+ankona@users.noreply.github.com>
2025-09-09 18:08:17 -0400

    Ensure mutable attributes are deep-copied, block reference updates

--- cstar/orchestration/models.py
+++ cstar/orchestration/models.py
@@ -1,12 +1,14 @@
 import typing as t
+from copy import deepcopy
 from enum import StrEnum, auto
 
-from pydantic import BaseModel, Field, FilePath, StringConstraints
+from pydantic import BaseModel, Field, FilePath, StringConstraints, field_validator
 
 RequiredString: t.TypeAlias = t.Annotated[
     str,
     StringConstraints(strip_whitespace=True, min_length=1),
 ]
+KeyValueStore: t.TypeAlias = dict[str, str | int]
 
 
 class WorkPlanState(StrEnum):
@@ -14,6 +16,7 @@ class WorkPlanState(StrEnum):
 
     Draft = auto()
     """A workflow that has not been validated."""
+
     Validated = auto()
     """A workflow that has been validated."""
 
@@ -40,29 +43,38 @@ class Step(BaseModel):
     """An optional list of external step names that must execute prior to this step."""
     blueprint: FilePath
     """The blueprint that will be executed in this step."""
-    depends_on: t.Annotated[list[RequiredString], Field(default_factory=list)] = []
+    depends_on: t.Annotated[
+        list[RequiredString],
+        Field(
+            default_factory=list,
+            frozen=True,
+        ),
+    ] = []
     """A collection of key-value pairs specifying overrides for blueprint attributes."""
     blueprint_overrides: t.Annotated[
-        dict[str, str | int],
+        KeyValueStore,
         Field(
             default_factory=dict,
             validate_default=False,
+            frozen=True,
         ),
     ] = {}
     """The user-friendly name of the application executed in the step."""
     compute_overrides: t.Annotated[
-        dict[str, str | int],
+        KeyValueStore,
         Field(
             default_factory=dict,
             validate_default=False,
+            frozen=True,
         ),
     ] = {}
     """A collection of key-value pairs specifying overrides for compute attributes."""
     workflow_overrides: t.Annotated[
-        dict[str, str | int],
+        KeyValueStore,
         Field(
             default_factory=dict,
             validate_default=False,
+            frozen=True,
         ),
     ] = {}
     """A collection of key-value pairs specifying overrides for workflow attributes."""
@@ -75,14 +87,23 @@ class WorkPlan(BaseModel):
     """The user-friendly name of the workplan."""
     description: RequiredString
     """A user-friendly description of the workplan."""
-    steps: t.Annotated[t.Sequence[Step], Field(default_factory=list, min_length=1)]
+    steps: t.Annotated[
+        t.Sequence[Step],
+        Field(
+            default_factory=list,
+            min_length=1,
+            frozen=True,
+        ),
+    ]
     """The steps to be executed by the workplan."""
     state: WorkPlanState = WorkPlanState.Draft
     """The current validation status of the workplan."""
     compute_environment: t.Annotated[
-        dict[str, str | int],
-        [str, str | int],
-        Field(default_factory=dict),
+        KeyValueStore,
+        Field(
+            default_factory=dict,
+            frozen=True,
+        ),
     ] = {}
     """A collection of key-value pairs specifying attributes for the target compute environment."""
     runtime_vars: t.Annotated[
@@ -90,6 +111,12 @@ class WorkPlan(BaseModel):
         Field(
             default_factory=list,
             validate_default=False,
+            frozen=True,
         ),
     ] = []
     """A collection of user-defined variables that will be populated at runtime."""
+
+    @field_validator("steps", mode="before")
+    @classmethod
+    def _deep_copy_steps(cls, value: list[Step]) -> list[Step]:
+        return deepcopy(value)
--- cstar/tests/unit_tests/orchestration/test_models.py
+++ cstar/tests/unit_tests/orchestration/test_models.py
@@ -9,7 +9,7 @@ import pytest
 import yaml
 from pydantic import BaseModel, ValidationError
 
-from cstar.orchestration.models import Step, WorkPlan, WorkPlanState
+from cstar.orchestration.models import KeyValueStore, Step, WorkPlan, WorkPlanState
 
 
 def model_to_yaml(model: BaseModel) -> str:
@@ -121,7 +121,11 @@ def test_step_defaults(fake_blueprint_path: Path) -> None:
     """
     step_name = f"test-step-{uuid.uuid4()}"
     app_name = f"test-app-{uuid.uuid4()}"
-    step = Step(name=step_name, application=app_name, blueprint=fake_blueprint_path)
+    step = Step(
+        name=step_name,
+        application=app_name,
+        blueprint=fake_blueprint_path,
+    )
 
     assert step.name == step_name
     assert step.application == app_name
@@ -179,6 +183,8 @@ def test_step_name_validation(
         "",
         "   ",
         None,
+        "\n",
+        "  \n  ",
     ],
 )
 def test_step_application_validation(
@@ -214,7 +220,7 @@ def test_step_application_validation(
     ],
 )
 def test_step_path_validation(invalid_value: Path | None) -> None:
-    """Verify the application field is populated with a non-null, non-whitespace string.
+    """Verify the path field is populated with a path to a file that exists.
 
     Parameters
     ----------
@@ -239,13 +245,18 @@ def test_step_path_validation(invalid_value: Path | None) -> None:
     [
         [""],
         [" ", "bbb"],
+        ["\n"],
+        ["  \n  "],
     ],
 )
-def test_step_depends_on_validation(
+def test_step_dependson_validation(
     fake_blueprint_path: Path,
     invalid_value: set[str],
 ) -> None:
-    """Verify that step depends_on value is stored as expected.
+    """Verify that step.depends_on does not allow invalid values.
+
+    - confirms that empty strings are not allowed
+    - confirms that whitespace is stripped
 
     Parameters
     ----------
@@ -265,6 +276,38 @@ def test_step_depends_on_validation(
     assert "depends_on" in str(error)
 
 
+def test_step_dependson_set(
+    fake_blueprint_path: pathlib.Path,
+) -> None:
+    """Verify that the Step does not allow the depends_on reference to change.
+
+    Parameters
+    ----------
+    fake_blueprint_path : Path
+        A path to a file that meets minimum expectations (it exists).
+
+    """
+    depends_on = ["a", "b", "c"]
+
+    step_name = f"test-step-{uuid.uuid4()}"
+    app_name = f"test-app-{uuid.uuid4()}"
+    step = Step(
+        name=step_name,
+        application=app_name,
+        blueprint=fake_blueprint_path,
+        depends_on=depends_on,
+    )
+
+    new_values = ["d", "e"]
+    depends_on.extend(new_values)
+
+    # sanity check that list is distinct
+    with pytest.raises(ValidationError) as error:
+        step.depends_on = depends_on
+
+    assert "depends_on" in str(error)
+
+
 @pytest.mark.parametrize(
     "depends_on",
     [
@@ -273,7 +316,7 @@ def test_step_depends_on_validation(
         ["aaa", "bbb"],
     ],
 )
-def test_step_depends_on(
+def test_step_dependson(
     fake_blueprint_path: Path,
     depends_on: list[str],
 ) -> None:
@@ -294,6 +337,49 @@ def test_step_depends_on(
     )
 
     assert step.depends_on == depends_on
+
+
+@pytest.mark.parametrize(
+    "depends_on",
+    [
+        [],
+        ["aaa"],
+        ["aaa", "bbb"],
+    ],
+)
+def test_step_dependson_copy(
+    fake_blueprint_path: Path,
+    depends_on: list[str],
+) -> None:
+    """Verify that step depends_on value is deep copied
+
+    Parameters
+    ----------
+    fake_blueprint_path : Path
+        A path to a file that meets minimum expectations (it exists).
+    depends_on: set[str]
+        A set of dependencies for the step
+    """
+    step = Step(
+        name="test-step",
+        application="test-app",
+        blueprint=fake_blueprint_path,
+        depends_on=depends_on,
+    )
+
+    new_values = ["1", "2", "3"]
+    og_value = set(depends_on)
+    depends_on.extend(new_values)
+
+    # sanity check that list is distinct
+    attr_value = step.depends_on
+    assert len(attr_value) == len(depends_on) - len(new_values)
+
+    # confirm vars are functionally the same
+    assert og_value == set(attr_value)
+
+    # confirm post-init changes are not propagated
+    assert not set(new_values).intersection(set(attr_value))
 
 
 @pytest.mark.parametrize(
@@ -360,6 +446,37 @@ def test_step_compute_overrides(
     assert step.compute_overrides == overrides
 
 
+def test_step_compute_overrides_set(
+    fake_blueprint_path: pathlib.Path,
+) -> None:
+    """Verify that the Step does not allow the compute overrides reference to change.
+
+    Parameters
+    ----------
+    fake_blueprint_path : Path
+        A path to a file that meets minimum expectations (it exists).
+
+    """
+    overrides = {"a": 1, "b": 2, "c": "xyz"}
+
+    step_name = f"test-step-{uuid.uuid4()}"
+    app_name = f"test-app-{uuid.uuid4()}"
+    step = Step(
+        name=step_name,
+        application=app_name,
+        blueprint=fake_blueprint_path,
+        compute_overrides=overrides,
+    )
+
+    new_values: KeyValueStore = {"CSTAR_XYZ": 42, "CSTAR_PQR": "xxx"}
+
+    # sanity check that list is distinct
+    with pytest.raises(ValidationError) as error:
+        step.compute_overrides = new_values
+
+    assert "compute_overrides" in str(error)
+
+
 @pytest.mark.parametrize(
     "overrides",
     [
@@ -390,6 +507,119 @@ def test_step_workflow_overrides(
     )
 
     assert step.workflow_overrides == overrides
+
+
+def test_step_workflow_overrides_set(
+    fake_blueprint_path: pathlib.Path,
+) -> None:
+    """Verify that the Step does not allow the workflow overrides reference to change.
+
+    Parameters
+    ----------
+    fake_blueprint_path : Path
+        A path to a file that meets minimum expectations (it exists).
+
+    """
+    overrides = {"a": 1, "b": 2, "c": "xyz"}
+
+    step_name = f"test-step-{uuid.uuid4()}"
+    app_name = f"test-app-{uuid.uuid4()}"
+    step = Step(
+        name=step_name,
+        application=app_name,
+        blueprint=fake_blueprint_path,
+        workflow_overrides=overrides,
+    )
+
+    new_values: KeyValueStore = {"CSTAR_XYZ": 42, "CSTAR_PQR": "xxx"}
+
+    # sanity check that list is distinct
+    with pytest.raises(ValidationError) as error:
+        step.workflow_overrides = new_values
+
+    assert "workflow_overrides" in str(error)
+
+
+@pytest.mark.parametrize(
+    "dict_prop",
+    [
+        "blueprint_overrides",
+        "compute_overrides",
+        "workflow_overrides",
+    ],
+)
+def test_step_all_overrides_copy(
+    fake_blueprint_path: pathlib.Path,
+    dict_prop: str,
+) -> None:
+    """Verify that the Step copy of blueprint_overrides is distinct
+
+    Parameters
+    ----------
+    fake_blueprint_path : Path
+        A path to a file that meets minimum expectations (it exists).
+    dict_prop: str
+        The name of the attribute on the Step object that should be tested.
+
+    """
+    overrides = {"a": 1, "b": 2, "c": "xyz"}
+
+    step_name = f"test-step-{uuid.uuid4()}"
+    app_name = f"test-app-{uuid.uuid4()}"
+    step = Step(
+        name=step_name,
+        application=app_name,
+        blueprint=fake_blueprint_path,
+        **{
+            dict_prop: overrides,
+        },  # type: ignore[reportArgumentType]
+    )
+
+    og_env = dict(**overrides)
+    new_values = {"CSTAR_XYZ": 42, "CSTAR_PQR": "xxx"}
+
+    overrides.update(new_values)
+
+    # sanity check that list is distinct
+    attr_value = getattr(step, dict_prop)
+    assert len(attr_value) == len(overrides) - len(new_values)
+
+    # confirm vars are functionally the same
+    assert og_env == attr_value
+
+    # confirm post-init changes are not propagated
+    assert not set(new_values).intersection(set(attr_value))
+
+
+def test_step_blueprint_overrides_set(
+    fake_blueprint_path: pathlib.Path,
+) -> None:
+    """Verify that the Step does not allow the blueprint overrides reference to change.
+
+    Parameters
+    ----------
+    fake_blueprint_path : Path
+        A path to a file that meets minimum expectations (it exists).
+
+    """
+    overrides = {"a": 1, "b": 2, "c": "xyz"}
+
+    step_name = f"test-step-{uuid.uuid4()}"
+    app_name = f"test-app-{uuid.uuid4()}"
+    step = Step(
+        name=step_name,
+        application=app_name,
+        blueprint=fake_blueprint_path,
+        blueprint_overrides=overrides,
+    )
+
+    new_values: KeyValueStore = {"CSTAR_XYZ": 42, "CSTAR_PQR": "xxx"}
+
+    # sanity check that list is distinct
+    with pytest.raises(ValidationError) as error:
+        step.blueprint_overrides = new_values
+
+    assert "blueprint_overrides" in str(error)
 
 
 def test_workplan_defaults(
@@ -681,3 +911,187 @@ def test_yaml_deserialize(
     plan2 = yaml_to_model(written, WorkPlan)
 
     assert plan == plan2
+
+
+def test_workplan_step_copy(
+    gen_fake_steps: t.Callable[[int], t.Generator[Step]],
+) -> None:
+    """Verify that the WorkPlan deep-copies steps.
+
+    Parameters
+    ----------
+    gen_fake_steps : t.Callable[[int], t.Generator[Step]]
+        A generator function to produce minimally valid test steps
+
+    """
+    steps = list(gen_fake_steps(2))
+    plan = WorkPlan(
+        name="test-plan",
+        description="test-description",
+        steps=steps,
+    )
+
+    step_names = {s.name for s in steps}
+
+    new_name = "changed-name"
+    steps[0].name = new_name
+
+    new_steps = list(gen_fake_steps(1))
+    steps.extend(new_steps)
+
+    # sanity check that list is copied
+    assert len(plan.steps) == len(steps) - len(new_steps)
+
+    # confirm steps are functionally the same
+    assert set(step_names) == {s.name for s in plan.steps}
+
+    # confirm post-init changes are not propagated
+    assert new_name not in {s.name for s in plan.steps}
+
+
+def test_workplan_step_set(
+    gen_fake_steps: t.Callable[[int], t.Generator[Step]],
+) -> None:
+    """Verify that the WorkPlan does not allow the steps list reference to change.
+
+    Parameters
+    ----------
+    gen_fake_steps : t.Callable[[int], t.Generator[Step]]
+        A generator function to produce minimally valid test steps
+
+    """
+    steps = list(gen_fake_steps(2))
+    plan = WorkPlan(
+        name="test-plan",
+        description="test-description",
+        steps=list(gen_fake_steps(1)),
+    )
+
+    with pytest.raises(ValidationError) as error:
+        plan.steps = steps
+
+    assert "steps" in str(error)
+
+
+def test_workplan_runtimevars_copy(
+    gen_fake_steps: t.Callable[[int], t.Generator[Step]],
+) -> None:
+    """Verify that the WorkPlan copy of runtime_vars is distinct
+
+    Parameters
+    ----------
+    gen_fake_steps : t.Callable[[int], t.Generator[Step]]
+        A generator function to produce minimally valid test steps
+
+    """
+    runtime_vars = ["a", "b", "c"]
+
+    plan = WorkPlan(
+        name="test-plan",
+        description="test-description",
+        steps=list(gen_fake_steps(2)),
+        runtime_vars=runtime_vars,
+    )
+
+    og_vars = set(runtime_vars)
+    new_vars = ["d", "e"]
+
+    runtime_vars.extend(new_vars)
+
+    # sanity check that list is distinct
+    assert len(plan.runtime_vars) == len(runtime_vars) - len(new_vars)
+
+    # confirm vars are functionally the same
+    assert og_vars == set(plan.runtime_vars)
+
+    # confirm post-init changes are not propagated
+    assert not set(new_vars).intersection(set(plan.runtime_vars))
+
+
+def test_workplan_runtimevars_set(
+    gen_fake_steps: t.Callable[[int], t.Generator[Step]],
+) -> None:
+    """Verify that the WorkPlan does not allow the runtime_vars list reference to change.
+
+    Parameters
+    ----------
+    gen_fake_steps : t.Callable[[int], t.Generator[Step]]
+        A generator function to produce minimally valid test steps
+
+    """
+    runtime_vars = ["a", "b", "c"]
+
+    plan = WorkPlan(
+        name="test-plan",
+        description="test-description",
+        steps=list(gen_fake_steps(2)),
+    )
+
+    with pytest.raises(ValidationError) as error:
+        plan.runtime_vars = runtime_vars
+
+    assert "runtime_vars" in str(error)
+
+
+def test_workplan_computeenv_copy(
+    gen_fake_steps: t.Callable[[int], t.Generator[Step]],
+) -> None:
+    """Verify that the WorkPlan copy of compute_environment is distinct
+
+    Parameters
+    ----------
+    gen_fake_steps : t.Callable[[int], t.Generator[Step]]
+        A generator function to produce minimally valid test steps
+
+    """
+    compute_env = {"a": 1, "b": 2, "c": "xyz"}
+
+    plan = WorkPlan(
+        name="test-plan",
+        description="test-description",
+        steps=list(gen_fake_steps(2)),
+        compute_environment=compute_env,
+    )
+
+    og_env = dict(**compute_env)
+    new_values = {"CSTAR_XYZ": 42, "CSTAR_PQR": "xxx"}
+
+    compute_env.update(new_values)
+
+    # sanity check that list is distinct
+    assert len(plan.compute_environment) == len(compute_env) - len(new_values)
+
+    # confirm vars are functionally the same
+    assert og_env == plan.compute_environment
+
+    # confirm post-init changes are not propagated
+    assert not set(new_values).intersection(set(plan.compute_environment))
+
+
+def test_workplan_computeenv_set(
+    gen_fake_steps: t.Callable[[int], t.Generator[Step]],
+) -> None:
+    """Verify that the WorkPlan does not allow the compute env reference to change.
+
+    Parameters
+    ----------
+    gen_fake_steps : t.Callable[[int], t.Generator[Step]]
+        A generator function to produce minimally valid test steps
+
+    """
+    compute_env: KeyValueStore = {"a": 1, "b": 2, "c": "xyz"}
+
+    plan = WorkPlan(
+        name="test-plan",
+        description="test-description",
+        steps=list(gen_fake_steps(2)),
+        compute_environment=compute_env,
+    )
+
+    new_values: KeyValueStore = {"CSTAR_XYZ": 42, "CSTAR_PQR": "xxx"}
+    compute_env.update(new_values)
+
+    with pytest.raises(ValidationError) as error:
+        plan.compute_environment = compute_env
+
+    assert "compute_environment" in str(error)
