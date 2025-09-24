8bc59a7270bef31e769243ea0460843e261eef43
Christopher McBride <3595025+ankona@users.noreply.github.com>
2025-09-24 17:11:17 -0400

    revert unused planner/orchestrator

--- cstar/tests/unit_tests/orchestration/test_serialization.py
+++ cstar/tests/unit_tests/orchestration/test_serialization.py
@@ -1,18 +1,11 @@
 import typing as t
 import uuid
 from pathlib import Path
-from unittest import mock
 
 import pytest
-from pydantic import BaseModel, ValidationError
+from pydantic import ValidationError
 
-from cstar.orchestration.models import Application, RomsMarblBlueprint, Workplan
-from cstar.orchestration.orchestrator import (
-    Launcher,
-    Orchestrator,
-    SerialPlanner,
-    TaskStatus,
-)
+from cstar.orchestration.models import Application, Workplan
 
 
 def test_workplan_no_data(
@@ -202,52 +195,3 @@ def test_workplan_runtime_vars(
     expected = set(expected_vars)
 
     assert actual == expected
-
-
-def test_serial_planner(
-    tmp_path: Path,
-    fill_blueprint_template: t.Callable[[dict[str, t.Any]], str],
-    deserialize_model: t.Callable[[Path, type], BaseModel],
-    complete_workplan_template_input: dict[str, t.Any],
-    fill_workplan_template: t.Callable[[dict[str, t.Any]], str],
-) -> None:
-    """Execute a workplan using a SerialPlanner and verify all tasks are executed."""
-    bp_yaml = fill_blueprint_template({"name": "Test Blueprint"})
-    bp_path = tmp_path / "blueprint.yaml"
-    bp_path.write_text(bp_yaml)
-
-    wp_yaml = fill_workplan_template(complete_workplan_template_input)
-    wp_path = tmp_path / "workplan.yaml"
-    wp_path.write_text(wp_yaml)
-
-    blueprint = t.cast(
-        "RomsMarblBlueprint",
-        deserialize_model(bp_path, RomsMarblBlueprint),
-    )
-    workplan = t.cast("Workplan", deserialize_model(wp_path, Workplan))
-
-    for step in workplan.steps:
-        # override frozen property with blueprint in this dir...
-        # todo: update workplan fixture to do this itself...
-        # must pass the steps and override the defaults from the fixture
-        setattr(step, "blueprint", blueprint)  # noqa: B010
-
-    planner = SerialPlanner(workplan, artifact_dir=tmp_path)
-    launcher = Launcher()
-    orchestrator = Orchestrator(planner, launcher)
-
-    num_steps = len(planner.plan)
-
-    with mock.patch.object(Orchestrator, "SLEEP_DURATION", 0.05):
-        orchestrator.run()
-
-    # confirm no tasks remain executing
-    assert not orchestrator.task_lookup
-    assert not planner.plan
-
-    # confirm all tasks in the plan were moved to archive on completion
-    assert orchestrator.task_archive
-    assert len(orchestrator.task_archive) == num_steps
-
-    # verify expected status for completed tasks
-    assert all(t.status == TaskStatus.Done for t in orchestrator.task_archive.values())
