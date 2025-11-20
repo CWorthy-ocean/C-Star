2e7850bfeee7b95380762c73e91c60fc378cf6bd
Chris McBride <3595025+ankona@users.noreply.github.com>
2025-11-20 04:18:48 -0500

    Fix local-launcher behavior

--- cstar/orchestration/launch/local.py
+++ cstar/orchestration/launch/local.py
@@ -1,10 +1,13 @@
+import asyncio
 import datetime
 import random
-import typing as t
-from subprocess import Popen
+from multiprocessing import Process as MpProcess
+from subprocess import run as sprun
 
-from psutil import NoSuchProcess, Process
+from psutil import NoSuchProcess
+from psutil import Process as PsProcess
 
+from cstar.base.exceptions import CstarExpectationFailed
 from cstar.orchestration.models import Step
 from cstar.orchestration.orchestration import (
     Launcher,
@@ -12,12 +15,18 @@ from cstar.orchestration.orchestration import (
     Status,
     Task,
 )
+from cstar.orchestration.utils import slugify
+
+
+def run_as_process(step: Step, cmd: list[str]) -> dict[str, int]:
+    p = sprun(args=cmd, text=True)
+    return {step.name: p.returncode}
 
 
 class LocalHandle(ProcessHandle):
     """Handle enabling reference to a task running in local processes."""
 
-    popen: Popen
+    process: MpProcess
     """The process handle (used only for simulating local processes)."""
     start_at: float
     """The process creation time as a posix timestamp (in seconds)."""
@@ -25,6 +34,7 @@ class LocalHandle(ProcessHandle):
     def __init__(
         self,
         step: Step,
+        process: MpProcess,
         pid: int,
         start_at: datetime.datetime | float,
     ) -> None:
@@ -39,13 +49,15 @@ class LocalHandle(ProcessHandle):
         start_at : datetime
             The process start time.
         """
+        super().__init__(pid=str(pid))
+
         self.step = step
+        self.process = process
         self.start_at = (
             start_at.timestamp()
             if isinstance(start_at, datetime.datetime)
             else start_at
         )
-        super().__init__(pid=str(pid))
 
     @property
     def elapsed(self) -> float:
@@ -62,15 +74,16 @@ class LocalHandle(ProcessHandle):
 class LocalLauncher(Launcher[LocalHandle]):
     """A launcher that executes steps in a local process."""
 
-    processes: t.ClassVar[dict[str, Popen]] = {}
-    """Mapping from step name to process."""
+    # processes: t.ClassVar[dict[str, MpProcess]] = {}
+    # """Mapping from step name to process."""
+    # schedule: dict[str, Step] = {}
 
-    @staticmethod
-    async def _update_processes() -> None:
-        """Update all process statuses."""
-        to_query = [p for p in LocalLauncher.processes.values() if p.returncode is None]
-        for process in to_query:
-            process.poll()
+    # @staticmethod
+    # async def _update_processes() -> None:
+    #     """Update all process statuses."""
+    #     # for process in [p for p in LocalLauncher.processes.values() if p is not None]:
+    #     #     process.poll()
+    #     ...
 
     @staticmethod
     async def _submit(step: Step, dependencies: list[LocalHandle]) -> LocalHandle:
@@ -85,44 +98,42 @@ class LocalLauncher(Launcher[LocalHandle]):
 
         Returns
         -------
-        LocalHandle
+        LocalHandle | None
             A ProcessHandle identifying the newly submitted job.
         """
-        # # TODO: implement dependency checking in local launcher correctly
-        # print(f"Checking deps for `{step.name}`: {[d.step.name for d in dependencies]}")
-        # tracked = {
-        #     d.step.name: (d.step, LocalLauncher.processes.get(d.step.name, None))
-        #     for d in dependencies
-        # }
-
-        await LocalLauncher._update_processes()
-
-        # if any(
-        #     process
-        #     for _, process in tracked.values()
-        #     if not process or (process and process.returncode is None)
-        #     # and process.create_time() == # TODO: solve for create date...
-        # ):
-        #     raise CstarExpectationFailed(
-        #         f"Unsatisfied prerequisites. Unable to start `{step.name}`."
-        #     )
-
-        cmd = ["sleep", str(random.randint(0, 3))]
+        cmd = ["sleep", str(random.randint(1, 4))]
         print(f"Creating local process from cmd: {' '.join(cmd)}")
 
-        start_at = datetime.datetime.now()
-        LocalLauncher.processes[step.name] = Popen(cmd)
-        pid = LocalLauncher.processes[step.name].pid
-
         try:
-            start_at = Process(pid).create_time()
-        except NoSuchProcess:
-            print(f"Unable to retrieve exact start time for completed pid: {pid}")
+            mp_process = MpProcess(
+                target=run_as_process,
+                name=slugify(step.name),
+                args=(step, cmd),
+                daemon=True,
+            )
+            mp_process.start()
+            create_time = datetime.datetime.now()
 
-        handle = LocalHandle(step, pid, start_at)
-        print(f"Local run of `{step.application}` created pid: {pid}")
+            if pid := mp_process.pid:
+                print(f"Local run of `{step.application}` created pid: {pid}")
 
-        return handle
+                try:
+                    ps_process = PsProcess(pid)
+                    create_timestamp = ps_process.create_time()
+                    create_time = datetime.datetime.fromtimestamp(create_timestamp)
+                except NoSuchProcess:
+                    print(f"Unable to retrieve exact start time for pid: {pid}")
+
+                return LocalHandle(
+                    step,
+                    mp_process,
+                    pid,
+                    create_time,
+                )
+        finally:
+            ...
+
+        raise RuntimeError("Unable to retrieve process ID for local process.")
 
     @staticmethod
     async def _status(step: Step, handle: LocalHandle) -> str:
@@ -140,11 +151,8 @@ class LocalLauncher(Launcher[LocalHandle]):
         str
             The current status of the step.
         """
-        await LocalLauncher._update_processes()
-
-        rc: int | None = None
-        if handle.step.name in LocalLauncher.processes:
-            rc = LocalLauncher.processes[handle.step.name].returncode
+        # await LocalLauncher._update_processes()
+        rc = handle.process.exitcode
 
         print(f"Return code for pid `{handle.pid}` is `{rc}` for `{step.name}`")
         if rc is None:
@@ -173,10 +181,28 @@ class LocalLauncher(Launcher[LocalHandle]):
         Task[LocalHandle]
             A Task containing information about the newly submitted job.
         """
+        tasks = [asyncio.Task(cls.query_status(h.step, h)) for h in dependencies]
+        statuses = await asyncio.gather(*tasks)
+        active_found = any(map(Status.is_running, statuses))
+        failure_found = any(map(Status.is_failure, statuses))
+
+        # wait for the dependencies to complete before launching
+        while active_found and not failure_found:
+            await asyncio.sleep(1)
+
+            tasks = [asyncio.Task(cls.query_status(h.step, h)) for h in dependencies]
+            statuses = await asyncio.gather(*tasks)
+            active_found = any(map(Status.is_running, statuses))
+            failure_found = any(map(Status.is_failure, statuses))
+
+        if failure_found:
+            raise CstarExpectationFailed(
+                f"Dependency of step {step.name} failed. Unable to continue."
+            )
+
         handle = await LocalLauncher._submit(step, dependencies)
-        status = await LocalLauncher.query_status(step, handle)
         return Task(
-            status=status,
+            status=Status.Submitted,
             step=step,
             handle=handle,
         )
@@ -226,16 +252,14 @@ class LocalLauncher(Launcher[LocalHandle]):
         Task[LocalHandle]
             The task after the cancellation attempt has completed.
         """
-        handle = item.handle
-        process = LocalLauncher.processes.get(item.step.name, None)
+        process = item.handle.process
 
-        if process and process.returncode is not None:
-            # can't cancel it if it's already done
-            print(f"Unable to cancel this running task `{handle.pid}")
-            return item
-
-        if process and process.returncode:
-            handle.popen.kill()
-            item.status = Status.Cancelled
+        if process is not None:
+            if process.exitcode is not None:
+                # can't cancel a completed process
+                print(f"Unable to cancel a completed task `{process.pid}")
+            else:
+                process.kill()
+                item.status = Status.Cancelled
 
         return item
