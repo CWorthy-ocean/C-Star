ca8967659ac385d09e5cab215b21b7059be1796b
Chris McBride <3595025+ankona@users.noreply.github.com>
2025-12-05 17:33:56 -0500

    Fix slurm-launcher-parameter-passing-todo

--- cstar/orchestration/launch/slurm.py
+++ cstar/orchestration/launch/slurm.py
@@ -136,6 +136,45 @@ app_to_cmd_map: dict[str, StepToCommandConversionFn] = {
 class SlurmLauncher(Launcher[SlurmHandle]):
     """A launcher that executes steps in a SLURM-enabled cluster."""
 
+    @staticmethod
+    def configured_queue() -> str:
+        """Get the queue to use for SLURM jobs.
+
+        Read from the environment variable `CSTAR_SLURM_QUEUE`.
+
+        Returns
+        -------
+        str
+            The queue to use for SLURM jobs.
+        """
+        return os.getenv("CSTAR_SLURM_QUEUE") or ""
+
+    @staticmethod
+    def configured_walltime() -> str:
+        """Get the max-walltime to use for SLURM jobs.
+
+        Read from the environment variable `CSTAR_SLURM_MAX_WALLTIME`.
+
+        Returns
+        -------
+        str
+            The max-walltime to use for SLURM jobs.
+        """
+        return os.getenv("CSTAR_SLURM_MAX_WALLTIME") or ""
+
+    @staticmethod
+    def configured_account() -> str:
+        """Get the account to use for SLURM jobs.
+
+        Read from the environment variable `CSTAR_SLURM_ACCOUNT`.
+
+        Returns
+        -------
+        str
+            The account to use for SLURM jobs.
+        """
+        return os.getenv("CSTAR_SLURM_ACCOUNT") or ""
+
     @task(persist_result=True, cache_key_fn=cache_key_func)
     @staticmethod
     async def _submit(step: Step, dependencies: list[SlurmHandle]) -> SlurmHandle:
@@ -170,18 +209,14 @@ class SlurmLauncher(Launcher[SlurmHandle]):
         command = step_converter(step)
         job = create_scheduler_job(
             commands=command,
-            account_key=os.getenv("CSTAR_SLURM_ACCOUNT", ""),
+            account_key=SlurmLauncher.configured_account(),
             cpus=bp.cpus_needed,
             nodes=None,  # let existing logic handle this
             cpus_per_node=None,  # let existing logic handle this
             script_path=bp.runtime_params.output_dir / "scripts" / f"{job_name}.sh",
             run_path=bp.runtime_params.output_dir,
-            job_name=job_name,
-            output_file=None,  # to fill with some convention
-            queue_name=os.getenv("CSTAR_SLURM_QUEUE"),
-            walltime=os.getenv(
-                "CSTAR_SLURM_MAX_WALLTIME", "48:00:00"
-            ),  # TODO how to determine this one?
+            queue_name=SlurmLauncher.configured_queue(),
+            walltime=SlurmLauncher.configured_walltime(),
             depends_on=job_dep_ids,
         )
 
