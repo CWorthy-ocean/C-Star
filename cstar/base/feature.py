cb883e9d37705ba187b561005017a2d44138ec50
Christopher McBride <3595025+ankona@users.noreply.github.com>
2025-12-12 17:47:19 -0500

    fix dev-mode key bug, convert constants to literals

--- cstar/base/feature.py
+++ cstar/base/feature.py
@@ -1,4 +1,10 @@
 import os
+from typing import Literal
+
+FLAG_DEVELOPER_MODE: Literal["CSTAR_FF_DEVELOPER_MODE"] = "CSTAR_FF_DEVELOPER_MODE"
+FLAG_PREFIX: Literal["CSTAR_FF_"] = "CSTAR_FF_"
+FF_ON: Literal["1"] = "1"
+FF_OFF: Literal["0"] = "0"
 
 
 def is_feature_enabled(flag: str) -> bool:
@@ -15,13 +21,12 @@ def is_feature_enabled(flag: str) -> bool:
         pass only the <FLAG_NAME> and exclude the `CSTAR_FF_` prefix.
     """
     # developer mode enables all feature flags
-    developer_mode = os.getenv("CSTAR_FF_DEVELOPER", "0")
-    if developer_mode == "1":
+    if os.getenv(FLAG_DEVELOPER_MODE, FF_OFF) == FF_ON:
         return True
 
     # enable omitting the CSTAR_FF_ prefix at the call-site
-    if not flag.startswith("CSTAR_FF_"):
-        flag = f"CSTAR_FF_{flag}"
+    if not flag.startswith(FLAG_PREFIX):
+        flag = f"{FLAG_PREFIX}{flag}"
 
     # Enable hierarchical feature flag segments - Given a flag:
     #   CSTAR_FF_CLI_BLUEPRINT_CHECK
@@ -34,7 +39,7 @@ def is_feature_enabled(flag: str) -> bool:
         for i in range(2, len(flag_parts)):
             segment_id = "_".join(flag_parts[:i])
 
-            if os.getenv(segment_id, "0") == "1":
+            if os.getenv(segment_id, FF_OFF) == FF_ON:
                 return True
 
-    return os.getenv(flag, "0") == "1"
+    return os.getenv(flag, FF_OFF) == FF_ON
