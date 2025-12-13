171e8dbd59e0f2e7d7a5bb52beebd35f1c116d06
Chris McBride <3595025+ankona@users.noreply.github.com>
2025-12-12 23:18:59 -0500

    move optional arg

--- cstar/cli/workplan/run.py
+++ cstar/cli/workplan/run.py
@@ -13,11 +13,11 @@ app = typer.Typer()
 @app.command()
 def run(
     path: t.Annotated[Path, typer.Argument(help="Path to a workplan file.")],
+    output_dir: t.Annotated[Path, typer.Argument(help="Path to a directory where outputs will be written.")],
     run_id: t.Annotated[
         str,
         typer.Option(help="The unique identifier for an execution of the workplan."),
     ] = "...",
-    output_dir: t.Annotated[Path, typer.Argument(help="Path to a directory where outputs will be written.")],
 ) -> None:
     """Execute a workplan.
 
