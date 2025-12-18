f9766caf3d098fc2e7b67afee1dead2b164c84ff
Chris McBride <3595025+ankona@users.noreply.github.com>
2025-12-18 15:11:04 -0500

    Fix improper-override-usage

--- cstar/execution/file_system.py
+++ cstar/execution/file_system.py
@@ -49,7 +49,6 @@ class JobFileSystem(LoggingMixin):
         self.logs_dir = self.root / self.LOGS_NAME
         self.output_dir = self.root / self.OUTPUT_NAME
 
-    @property
     def _dir_set(self) -> set[Path]:
         """Return the complete set of directories for the job."""
         return {
@@ -62,7 +61,7 @@ class JobFileSystem(LoggingMixin):
 
     def prepare(self) -> None:
         """Ensure that the desired directories exist."""
-        for task_dir in self._dir_set:
+        for task_dir in self._dir_set():
             task_dir.mkdir(parents=True, exist_ok=True)
             self.log.debug(f"Created {task_dir.name} directory `{task_dir}`")
 
@@ -112,9 +111,8 @@ class RomsJobFileSystem(JobFileSystem):
         self.joined_output_dir = self.output_dir / RomsJobFileSystem.JOINED_OUTPUT_NAME
 
     @override
-    @property
     def _dir_set(self) -> set[Path]:
-        return super()._dir_set.union(
+        return super()._dir_set().union(
             {
                 self.compile_time_code_dir,
                 self.runtime_code_dir,
