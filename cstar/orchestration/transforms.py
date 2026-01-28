3084652248bb69d4f3e17151326d2b0a6674ab00
Chris McBride <3595025+ankona@users.noreply.github.com>
2026-01-27 19:21:34 -0500

    brevity and hack message consolidation with entire block

--- cstar/orchestration/transforms.py
+++ cstar/orchestration/transforms.py
@@ -258,6 +258,27 @@ class WorkplanTransformer(LoggingMixin):
 
         return directory / filename
 
+    def _ensure_unique_paths(self, steps: list[Step]) -> None:
+        """Modify any steps that do not include the unique run-id in their
+        output directory.
+
+        Parameters
+        ----------
+        steps : list[Step]
+            The steps to be modified.
+        """
+        # HACK: force-update the step output directories to avoid collisions
+        for step in steps:
+            bp = deserialize(step.blueprint_path, RomsMarblBlueprint)
+            overrides = step.blueprint_overrides
+            if "runtime_params" not in overrides:
+                step.blueprint_overrides["runtime_params"] = {}
+            runtime_params = t.cast(
+                dict[str, Path], overrides.get("runtime_params", {})
+            )
+            if "output_dir" not in runtime_params:
+                runtime_params["output_dir"] = step.working_dir(bp)
+
     def apply(self) -> Workplan:
         """Create a new workplan with appropriate transforms applied.
 
@@ -284,17 +305,7 @@ class WorkplanTransformer(LoggingMixin):
 
                 steps.extend(transformed_steps)
 
-        # HACK: force-update the step output directories to avoid collisions
-        for step in self.original.steps:
-            bp = deserialize(step.blueprint_path, RomsMarblBlueprint)
-            overrides = step.blueprint_overrides
-            if "runtime_params" not in overrides:
-                step.blueprint_overrides["runtime_params"] = {}
-            runtime_params = t.cast(
-                dict[str, Path], overrides.get("runtime_params", {})
-            )
-            if "output_dir" not in runtime_params:
-                runtime_params["output_dir"] = step.working_dir(bp)
+        self._ensure_unique_paths(steps)  # HACK: remove when possible
 
         if is_feature_enabled("CSTAR_FF_ORCH_TRANSFORM_OVR"):
             override_transform = OverrideTransform()
