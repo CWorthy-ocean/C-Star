f1e69991ae9f123afae79cb33cb328b13d655344
Christopher McBride <3595025+ankona@users.noreply.github.com>
2026-02-18 14:50:56 -0500

    Fix string conversion in log writing useless log entry

--- cstar/io/retriever.py
+++ cstar/io/retriever.py
@@ -83,9 +83,11 @@ class Retriever(ABC, LoggingMixin):
         else:
             target_dir.mkdir(parents=True)
 
-        self.log.debug(f"Saving source `{self.source}` to: {target_dir}")
-        savepath = self._save(target_dir=target_dir)
-        return savepath
+        loc, has = self.source.location, self.source.checkout_hash
+        msg = f"Saving source `{loc}{f'@{has}' if has else ''}` to `{target_dir}`"
+        self.log.debug(msg)
+
+        return self._save(target_dir=target_dir)
 
     @abstractmethod
     def _save(self, target_dir: Path) -> Path:
