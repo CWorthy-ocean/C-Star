f4a59def4249abe62985854ce51bd2b33b97ca8b
Chris McBride <3595025+ankona@users.noreply.github.com>
2026-02-25 17:06:03 -0500

    Add more tests for input validation via CLI

---
+++ cstar/tests/unit_tests/orchestration/cli/blueprint/test_check_blueprint.py
@@ -0,0 +1,247 @@
+from pathlib import Path
+
+import pytest
+
+from cstar.cli.blueprint.check import check
+
+
+def test_blueprint_check_file_dne(
+    capsys: pytest.CaptureFixture,
+    tmp_path: Path,
+) -> None:
+    """Verify that a path to a non-existent blueprint fails a validity check.
+
+    Parameters
+    ----------
+    capsys : pytest.CaptureFixture
+        Used to verify outputs from the CLI
+    tmp_path : Path
+        Temporary directory to read/write test inputs and outputs
+    """
+    blueprint_path = tmp_path / "blueprint-dne.yml"
+
+    check(blueprint_path)
+
+    assert "not found" in capsys.readouterr().out
+
+
+def test_blueprint_check_file_no_content(
+    capsys: pytest.CaptureFixture,
+    tmp_path: Path,
+) -> None:
+    """Verify that an empty blueprint file fails a validity check.
+
+    Parameters
+    ----------
+    capsys : pytest.CaptureFixture
+        Used to verify outputs from the CLI
+    tmp_path : Path
+        Temporary directory to read/write test inputs and outputs
+    """
+    blueprint_path = tmp_path / "empty_blueprint.yml"
+    blueprint_path.touch()
+
+    check(blueprint_path)
+
+    assert "is invalid" in capsys.readouterr().out
+
+
+@pytest.mark.parametrize(
+    "content",
+    [" ", "", "\n", '{"foo": "bar"}'],
+)
+def test_blueprint_check_file_bad_content(
+    capsys: pytest.CaptureFixture,
+    tmp_path: Path,
+    content: str,
+) -> None:
+    """Verify that an empty blueprint fails a validity check.
+
+    Parameters
+    ----------
+    capsys : pytest.CaptureFixture
+        Used to verify outputs from the CLI
+    tmp_path : Path
+        Temporary directory to read/write test inputs and outputs
+    """
+    blueprint_path = tmp_path / "invalid_blueprint.yml"
+    blueprint_path.write_text(content)
+
+    check(blueprint_path)
+
+    assert "is invalid" in capsys.readouterr().out
+
+
+@pytest.mark.parametrize(
+    "repo_relative_path",
+    [
+        Path("cstar/tests/integration_tests/blueprints/blueprint_template.yaml"),
+        Path("cstar/tests/integration_tests/blueprints/blueprint_complete.yaml"),
+        Path("docs/tutorials/wales_toy_blueprint.yaml"),
+        Path("docs/tutorials/wio_toy_blueprint.yaml"),
+    ],
+)
+def test_blueprint_valid_input(
+    capsys: pytest.CaptureFixture,
+    repo_relative_path: Path,
+    package_path: Path,
+) -> None:
+    """Verify that a valid blueprint passes the CLI check.
+
+    NOTE: This test also serves a practical purpose of confirming the continued
+    validity of tutorial and sample blueprints.
+
+    Parameters
+    ----------
+    capsys : pytest.CaptureFixture
+        Used to verify outputs from the CLI
+    repo_relative_path : Path
+        Relative path to a blueprint within the c-star repo
+    package_path : Path
+        Absolute path to the c-star package on disk
+    """
+    blueprint_path = package_path / repo_relative_path
+
+    check(Path(blueprint_path))
+
+    assert "is valid" in capsys.readouterr().out, (
+        f"`{blueprint_path}` does not contain a valid blueprint"
+    )
+
+
+@pytest.mark.parametrize(
+    ("start_removal", "end_removal"),
+    [
+        ("name:", None),
+        ("description:", None),
+        ("valid_start_date:", None),
+        ("valid_end_date:", None),
+        ("code:", "grid:"),
+        ("grid:", "initial_conditions:"),
+        ("initial_conditions:", "forcing"),
+        ("forcing:", "partitioning"),
+        ("partitioning:", "model_params:"),
+        ("model_params:", "runtime_params:"),
+        ("runtime_params:", "<EOF>"),
+        ("run_time:", "compile_time:"),
+        ("compile_time:", "grid:"),
+        ("location:", None),
+        ("branch", None),
+        ("roms:", "marbl:"),
+        ("directory:", None),
+    ],
+)
+def test_blueprint_incomplete_input(
+    capsys: pytest.CaptureFixture,
+    start_removal: str,
+    end_removal: str,
+    tests_path: Path,
+    tmp_path: Path,
+) -> None:
+    """Verify that an incomplete blueprint fails the CLI check.
+
+    Starts with a sample blueprint and removes a piece of required information in each test.
+
+    Parameters
+    ----------
+    capsys : pytest.CaptureFixture
+        Used to verify outputs from the CLI
+    start_removal : Path
+        A string that will trigger content skipping to begin when building a test blueprint
+    end_removal : Path
+       A string that will trigger content skipping to end when building a test blueprint
+    tests_path : Path
+        Path to the c-star tests directory
+    tmp_path : Path
+        Temporary directory to read/write test inputs and outputs
+    """
+    blueprint_path = tests_path / "integration_tests/blueprints/blueprint_complete.yaml"
+
+    content = blueprint_path.read_text().splitlines()
+    remaining_content = []
+    cutting = False
+    cut_once = False
+
+    for line in content:
+        if start_removal in line and not cut_once:
+            cutting = True
+            cut_once = True
+        elif end_removal and end_removal in line:
+            cutting = False
+
+        if not cutting:
+            remaining_content.append(line)
+
+        if end_removal is None or end_removal in line:
+            cutting = False
+            remaining_content.append(line)
+
+    bp_path = tmp_path / "bp.yaml"
+    bp_path.write_text("\n".join(remaining_content))
+
+    check(bp_path)
+
+    err_msg = f"{bp_path} should not pass validation"
+    assert "is invalid" in capsys.readouterr().out, err_msg
+
+
+@pytest.mark.parametrize(
+    ("start_removal", "end_removal"),
+    [
+        ("application:", None),
+        ("state:", None),
+        ("filter:", "compile_time"),
+        ("files:", "directory:"),
+    ],
+)
+def test_blueprint_optional_input(
+    capsys: pytest.CaptureFixture,
+    start_removal: str,
+    end_removal: str,
+    tests_path: Path,
+    tmp_path: Path,
+) -> None:
+    """Verify that an incomplete blueprint passes the CLI check if the missing information
+    is not required.
+
+    Starts with a sample blueprint and removes a piece of optional information in each test.
+
+    Parameters
+    ----------
+    capsys : pytest.CaptureFixture
+        Used to verify outputs from the CLI
+    start_removal : Path
+        A string that will trigger content skipping to begin when building a test blueprint
+    end_removal : Path
+       A string that will trigger content skipping to end when building a test blueprint
+    tests_path : Path
+        Path to the c-star tests directory
+    tmp_path : Path
+        Temporary directory to read/write test inputs and outputs
+    """
+    blueprint_path = tests_path / "integration_tests/blueprints/blueprint_complete.yaml"
+
+    content = blueprint_path.read_text().splitlines()
+    remaining_content = []
+    cutting, cut_once = False, False
+
+    for line in content:
+        if start_removal in line and not cut_once:
+            cutting = True
+            cut_once = True
+        elif end_removal and end_removal in line:
+            cutting = False
+
+        if not cutting:
+            remaining_content.append(line)
+
+        if end_removal is None or end_removal in line:
+            cutting = False
+
+    bp_path = tmp_path / "bp.yaml"
+    bp_path.write_text("\n".join(remaining_content))
+
+    check(bp_path)
+
+    err_msg = f"{bp_path} should not pass validation"
+    assert "is valid" in capsys.readouterr().out, err_msg
--- cstar/tests/unit_tests/orchestration/cli/workplan/test_check.py
+++ cstar/tests/unit_tests/orchestration/cli/workplan/test_check.py
@@ -35,3 +35,242 @@ def test_cli_workplan_check_action_tpl(
     check(wp_path)
     captured = capsys.readouterr().out
     assert " valid" in captured
+
+
+def test_cli_workplan_check_dne(
+    tmp_path: Path,
+    capsys: pytest.CaptureFixture,
+) -> None:
+    """Verify that an invalid path fails a validity check.
+
+    Parameters
+    ----------
+    tmp_path : Path
+        Temporary directory for test outputs
+    workplan_name : str
+        The name of a workplan template to use for workplan creation
+    wp_templates_dir : Path
+        Fixture returning the path to the directory containing workplan template files
+    """
+    wp_path = tmp_path / "workplan-dne.yaml"
+
+    check(wp_path)
+    captured = capsys.readouterr().out
+    assert " not found" in captured
+
+
+def test_cli_workplan_check_file_no_content(
+    capsys: pytest.CaptureFixture,
+    tmp_path: Path,
+) -> None:
+    """Verify that an empty workplan file fails a validity check.
+
+    Parameters
+    ----------
+    capsys : pytest.CaptureFixture
+        Used to verify outputs from the CLI
+    tmp_path : Path
+        Temporary directory to read/write test inputs and outputs
+    """
+    wp_path = tmp_path / "empty_workplan.yml"
+    wp_path.touch()
+
+    check(wp_path)
+
+    assert "is invalid" in capsys.readouterr().out
+
+
+@pytest.mark.parametrize(
+    "content",
+    [" ", "", "\n", '{"foo": "bar"}', "name: Sample Workplan\n"],
+)
+def test_cli_workplan_check_file_bad_content(
+    capsys: pytest.CaptureFixture,
+    tmp_path: Path,
+    content: str,
+) -> None:
+    """Verify that an invalid/malformed workplan fails a validity check.
+
+    Parameters
+    ----------
+    capsys : pytest.CaptureFixture
+        Used to verify outputs from the CLI
+    tmp_path : Path
+        Temporary directory to read/write test inputs and outputs
+    """
+    wp_path = tmp_path / "invalid_workplan.yml"
+    wp_path.write_text(content)
+
+    check(wp_path)
+
+    assert "is invalid" in capsys.readouterr().out
+
+
+@pytest.mark.parametrize(
+    "repo_relative_path",
+    [
+        Path("docs/tutorials/workplan_laptop_example.yaml"),
+        Path("cstar/additional_files/templates/wp/workplan.yaml"),
+    ],
+)
+def test_cli_workplan_check_valid_input(
+    capsys: pytest.CaptureFixture,
+    repo_relative_path: Path,
+    package_path: Path,
+) -> None:
+    """Verify that a valid workplan passes the CLI check.
+
+    NOTE: This test also serves a practical purpose of confirming the continued
+    validity of tutorial and sample workplans.
+
+    Parameters
+    ----------
+    capsys : pytest.CaptureFixture
+        Used to verify outputs from the CLI
+    repo_relative_path : Path
+        Relative path to a workplan within the c-star repo
+    package_path : Path
+        Absolute path to the c-star package on disk
+    """
+    wp_path = package_path / repo_relative_path
+
+    check(Path(wp_path))
+
+    msg = f"`{wp_path}` does not contain a valid workplan"
+    assert "is valid" in capsys.readouterr().out, msg
+
+
+@pytest.mark.parametrize(
+    ("start_removal", "end_removal"),
+    [
+        ("name:", None),
+        ("description:", None),
+        ("steps:", "<EOF>"),
+        ("Prepare", "Ensemble X"),
+        ("Ensemble X", "Ensemble Y"),
+        ("Ensemble Y", "Aggregate"),
+        ("blueprint:", None),
+        ("segment_length:", None),
+    ],
+)
+def test_workplan_incomplete_input(
+    capsys: pytest.CaptureFixture,
+    start_removal: str,
+    end_removal: str,
+    tmp_path: Path,
+    package_path: Path,
+) -> None:
+    """Verify that an incomplete workplan fails the CLI check.
+
+    Starts with a sample workplan and removes a piece of required information in each test.
+
+    Parameters
+    ----------
+    capsys : pytest.CaptureFixture
+        Used to verify outputs from the CLI
+    start_removal : Path
+        A string that will trigger content skipping to begin when building a test workplan
+    end_removal : Path
+       A string that will trigger content skipping to end when building a test workplan
+    tmp_path : Path
+        Temporary directory to read/write test inputs and outputs
+    package_path : Path
+        Absolute path to the c-star package on disk
+    """
+    wp_path = package_path / "cstar/additional_files/templates/wp/workplan.yaml"
+
+    content = wp_path.read_text().splitlines()
+    remaining_content = []
+    cutting = False
+    cut_once = False
+
+    for line in content:
+        if start_removal in line and not cut_once:
+            cutting = True
+            cut_once = True
+        elif end_removal and end_removal in line:
+            cutting = False
+
+        if not cutting:
+            remaining_content.append(line)
+
+        if end_removal is None or end_removal in line:
+            cutting = False
+
+    bp_path = tmp_path / "bp.yaml"
+    bp_path.write_text("\n".join(remaining_content))
+
+    check(bp_path)
+
+    err_msg = f"{bp_path} should not pass validation"
+    assert "is invalid" in capsys.readouterr().out, err_msg
+
+
+@pytest.mark.parametrize(
+    ("start_removal", "end_removal"),
+    [
+        ("state:", None),
+        ("compute_environment:", "runtime_vars:"),
+        ("num_nodes:", None),
+        ("num_cpus_per_process:", None),
+        ("runtime_vars:", None),
+        ("Aggregate", None),
+        ("workflow_overrides:", "compute_overrides:"),
+        ("compute_overrides:", "name:"),
+        ("walltime", None),
+        ("          num_nodes:", None),
+        ("blueprint_overrides:", "workflow_overrides"),
+        ("depends_on:", "blueprint"),
+    ],
+)
+def test_workplan_optional_input(
+    capsys: pytest.CaptureFixture,
+    start_removal: str,
+    end_removal: str | None,
+    tmp_path: Path,
+    package_path: Path,
+) -> None:
+    """Verify that an incomplete workplan fails the CLI check.
+
+    Starts with a sample workplan and removes a piece of required information in each test.
+
+    Parameters
+    ----------
+    capsys : pytest.CaptureFixture
+        Used to verify outputs from the CLI
+    start_removal : Path
+        A string that will trigger content skipping to begin when building a test workplan
+    end_removal : Path
+       A string that will trigger content skipping to end when building a test workplan
+    tmp_path : Path
+        Temporary directory to read/write test inputs and outputs
+    package_path : Path
+        Absolute path to the c-star package on disk
+    """
+    wp_path = package_path / "cstar/additional_files/templates/wp/workplan.yaml"
+
+    content = wp_path.read_text().splitlines()
+    remaining_content = []
+    cutting = False
+    cut_once = False
+
+    for line in content:
+        if start_removal in line and not cut_once:
+            cutting = True
+            cut_once = True
+        elif end_removal and end_removal in line:
+            cutting = False
+
+        if not cutting:
+            remaining_content.append(line)
+
+        if end_removal is None or end_removal in line:
+            cutting = False
+
+    bp_path = tmp_path / "bp.yaml"
+    bp_path.write_text("\n".join(remaining_content))
+
+    check(bp_path)
+
+    err_msg = f"{bp_path} should not pass validation"
+    assert "is valid" in capsys.readouterr().out, err_msg
