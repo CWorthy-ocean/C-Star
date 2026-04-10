f775f28840469461c33804425c7217fc3006316a
Scott Eilerman <scott.eilerman@cworthy.org>
2026-04-10 15:46:00 -0600

    do both

--- cstar/tests/conftest.py
+++ cstar/tests/conftest.py
@@ -1,8 +1,11 @@
+import multiprocessing as mp
 from collections.abc import Generator
 from pathlib import Path
 from unittest.mock import Mock, patch
 
 import pytest
+
+mp.set_start_method("spawn")
 
 
 @pytest.fixture(scope="session")
