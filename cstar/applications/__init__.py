c7214874ff172dadae427ddad571b7a1500a249b
Scott Eilerman <scott.eilerman@cworthy.org>
2026-04-28 15:44:01 -0600

    remove commented code

--- cstar/applications/__init__.py
+++ cstar/applications/__init__.py
@@ -7,6 +7,3 @@ __all__ = [
     for f in modules
     if isfile(f) and not f.endswith("__init__.py") and not f.endswith("utils.py")
 ]
-# import importlib
-# for module in [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py') and not f.endswith('utils.py')]:
-#     importlib.import_module(f"cstar.applications.{module}")
