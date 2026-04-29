7b5a95d0f2105a7d8f5d4ca86fc3613501075310
Christopher McBride <3595025+ankona@users.noreply.github.com>
2026-04-29 18:53:42 -0400

    Add file DNE tests for directive file

--- cstar/tests/unit_tests/orchestration/cli/blueprint/test_run_blueprint.py
+++ cstar/tests/unit_tests/orchestration/cli/blueprint/test_run_blueprint.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 from unittest import mock
 
+import pytest
 from typer.testing import CliRunner
 
 from cstar.cli.blueprint.run import app
+from cstar.entrypoint.utils import ARG_DIRECTIVES_URI_LONG
 
 
 def test_blueprint_run_file_dne(tmp_path: Path) -> None:
@@ -63,7 +65,57 @@ def test_blueprint_run_remote_blueprint() -> None:
     mock_exec.assert_called_once()
 
 
-def test_blueprint_run_apply_directives() -> None:
+@pytest.mark.parametrize(
+    "directive_path",
+    [
+        "directive-dne.json",
+        "https://www.google.com/directive-dne.json",
+    ],
+)
+def test_blueprint_run_apply_directive_dne(tmp_path: Path, directive_path: str) -> None:
+    """Verify that an exception is raised if a path to a non-existent directive file is passed."""
+    bp_path = "https://raw.githubusercontent.com/CWorthy-ocean/cstar_blueprint_roms_marbl_example/refs/heads/main/wales-toy-domain/wales_toy_blueprint.yaml"
+
+    with mock.patch(
+        "cstar.cli.blueprint.run.exec_romsmarbl_runner",
+        return_value=0,
+    ):
+        runner = CliRunner()
+        result = runner.invoke(
+            app,
+            [
+                bp_path,
+                ARG_DIRECTIVES_URI_LONG,
+                directive_path,
+            ],
+            color=False,
+        )
+
+    assert "file not found" in result.stderr
+
+
+def test_blueprint_run_apply_directive_empty(tmp_path: Path) -> None:
+    """Verify that an exception is raised if an empty directive file is passed."""
+    bp_path = "https://raw.githubusercontent.com/CWorthy-ocean/cstar_blueprint_roms_marbl_example/refs/heads/main/wales-toy-domain/wales_toy_blueprint.yaml"
+    directive_file_path = tmp_path / "directive-dne.json"
+    directive_file_path.touch()
+
+    with mock.patch(
+        "cstar.cli.blueprint.run.exec_romsmarbl_runner",
+        return_value=0,
+    ):
+        runner = CliRunner()
+        result = runner.invoke(
+            app,
+            [
+                bp_path,
+                ARG_DIRECTIVES_URI_LONG,
+                directive_file_path.as_posix(),
+            ],
+            color=False,
+        )
+
+    assert "malformed" in result.stderr
     """Verify that a URL to a remote blueprint is handled properly and the
     blueprint is executed.
     """
