8e9bd501528af7280a13aa3ad11ca465d34574d2
Scott Eilerman <scott.eilerman@cworthy.org>
2026-05-13 13:56:19 -0600

    rework for rebase

---
+++ cstar/applications/nest_ic_app.py
@@ -0,0 +1,123 @@
+import typing as t
+from pathlib import Path
+
+import xarray as xr
+from roms_tools import Grid, InitialConditions
+
+from cstar.applications.core import (
+    ApplicationDefinition,
+    XRunnerResult,
+    register_application,
+)
+from cstar.applications.roms_marbl.transforms import RestartFile
+from cstar.base.log import get_logger
+from cstar.entrypoint.runner import XBlueprintRunner
+from cstar.execution.handler import ExecutionStatus
+from cstar.orchestration.models import Blueprint
+
+APP_NAME: t.Literal["nest_ic"] = "nest_ic"
+"""The unique identifier for the nest_ic application type."""
+_APP_NAME_LONG: t.Literal["Nesting Data Processor"] = "Nesting Data Processor"
+"""The long-form application name."""
+
+log = get_logger(__name__)
+
+
+class NestIcBlueprint(Blueprint):
+    """A blueprint for an example plotting application."""
+
+    application: str = APP_NAME
+    """The application identifier."""
+    parent_rst: Path
+    parent_grid: Path
+    child_grid: Path
+    output_dir: Path
+
+
+class NestIcRunner(XBlueprintRunner[NestIcBlueprint]):
+    """Worker class to execute a simple plotting application.
+
+    This application is intended primarily as an example of how to build a functioning
+    application to perform a custom task, rather than a fully-featured plotting utility
+    intended for scientific use.
+    """
+
+    application: str = APP_NAME
+    """The application identifier."""
+
+    @t.override
+    async def run(self) -> XRunnerResult[NestIcBlueprint]:
+        """Process the blueprint.
+
+        Returns
+        -------
+        RunnerResult
+            The result of the blueprint processing.
+        """
+        self.log.trace("Executing handler function on blueprint runner")
+        self.log.info(f"Running nest ic application for {self.blueprint}")
+
+        self._create_initial_conditions()
+        return self.set_state(ExecutionStatus.COMPLETED)
+
+    @staticmethod
+    def _has_bgc(filepath):
+        """Check if the parent restart file has BGC tracers."""
+        with xr.open_dataset(filepath) as ds:
+            # Example: check if nitrate, DIC exist
+            # These are specific to MARBL but other than capitalization are likely to be in any BGC or mCDR model
+            bgc_vars = ["NO3", "DIC", "no3", "dic"]
+            return any(var in ds.variables for var in bgc_vars)
+
+    def _create_initial_conditions(
+        self,
+    ):
+
+        rst = RestartFile(path=self.blueprint.restart_file)
+        restart_date = rst.timestamp
+
+        parent_grid = Grid(filename=self.blueprint.parent_grid_path)
+
+        # build InitialConditions kwargs
+        ic_kwargs = dict(
+            grid=Grid(filename=self.blueprint.child_grid_path),
+            ini_time=restart_date,
+            source={
+                "name": "ROMS",
+                "grid": parent_grid,
+                "path": self.blueprint.restart_file,
+            },
+            use_dask=True,
+            # model_reference_date=model_reference_date
+        )
+
+        # only add bgc_source if parent has BGC
+        if self._has_bgc(self.blueprint.restart_file):
+            ic_kwargs["bgc_source"] = {
+                "name": "ROMS",
+                "grid": parent_grid,
+                "path": self.blueprint.restart_file,
+            }
+
+        fname = f"ic_from_parent_rst.{rst.formatted_timestamp}.nc"
+        path = Path(self.blueprint.output_dir).expanduser() / fname
+        path.parent.mkdir(parents=True, exist_ok=True)
+
+        ic = InitialConditions(**ic_kwargs)
+
+        # Write out initial conditions file for child
+
+        ic.save(path)
+
+        return path
+
+
+@register_application
+class NestIcApplication(
+    ApplicationDefinition[NestIcBlueprint, NestIcRunner],
+):
+    name = APP_NAME
+    long_name = _APP_NAME_LONG
+    runner = NestIcRunner
+    blueprint = NestIcBlueprint
+    applicable_transforms = ()
