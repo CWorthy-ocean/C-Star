493ba27032a4cb5118f319c31e0086ae35c8964e
Scott Eilerman <scott.eilerman@cworthy.org>
2026-05-19 09:52:25 -0600

    add clear for workdir

--- cstar/execution/file_system.py
+++ cstar/execution/file_system.py
@@ -356,6 +356,7 @@ class RomsFileSystemManager(JobFileSystemManager):
             self.input_datasets_dir,
             self._codebases_dir,
             self.joined_output_dir,
+            self.work_dir,
         ]:
             if directory.exists():
                 shutil.rmtree(directory)
