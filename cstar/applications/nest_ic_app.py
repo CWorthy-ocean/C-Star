fd7022092f74f2ed5847fbec8fb0313b61f94a69
Christopher McBride <3595025+ankona@users.noreply.github.com>
2026-06-15 18:42:00 -0400

    Update cstar/applications/nest_ic_app.py

--- cstar/applications/nest_ic_app.py
+++ cstar/applications/nest_ic_app.py
@@ -32,6 +32,7 @@ class NestIcBlueprint(Blueprint):
     """The application identifier."""
     parent_rst: Path
     parent_grid: Path
+    """Path to a netCDF file defining the parent grid attributes."""
     child_grid: Path
     """Path to a netCDF file defining the child grid attributes."""
     output_dir: Path
