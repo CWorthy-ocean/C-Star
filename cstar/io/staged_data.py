568561bd72c236869869a99599a8826459088aa9
Scott Eilerman <scott.eilerman@cworthy.org>
2026-07-24 13:12:39 -0600

    fix suggestion

--- cstar/io/staged_data.py
+++ cstar/io/staged_data.py
@@ -131,26 +131,18 @@ class StagedFile(StagedData):
         if not resolved_path.exists():
             return True
 
-if stat := self._stat:
-    r_stat = os.stat(resolved_path.resolve())
+        if stat := self._stat:
+            r_stat = os.stat(resolved_path.resolve())
 
-    if stat.st_mtime != r_stat.st_mtime:
-        return True
+            if stat.st_mtime != r_stat.st_mtime:
+                return True
 
-    if stat.st_size != r_stat.st_size:
-        return True 
-        if (
-            self._stat
-            and self._stat.st_mtime != os.stat(resolved_path.resolve()).st_mtime
-        ):
-            return True
-        if (
-            self._stat
-            and self._stat.st_size != os.stat(resolved_path.resolve()).st_size
-        ):
-            return True
+            if stat.st_size != r_stat.st_size:
+                return True
+
         if self._sha256 and self._sha256 != _get_sha256_hash(resolved_path.resolve()):
             return True
+
         return False
 
     def _clear_cache(self):
