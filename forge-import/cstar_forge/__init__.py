1fbfef5c998276679006fa43c7198f3bfcc25d7a
Scott Eilerman <scott.eilerman@cworthy.org>
2026-08-03 14:22:45 -0600

    silence ucx warnings

--- forge-import/cstar_forge/__init__.py
+++ forge-import/cstar_forge/__init__.py
@@ -3,6 +3,11 @@ cstar_forge: A utility for generating regional oceanographic modeling domains
 and spawning reproducible C-Star workflows.
 """
 
+# silence UCX warnings
+import os
+
+os.environ["UCX_LOG_LEVEL"] = "error"
+
 from cstar_forge import catalog, config, diagnostics, models
 from cstar_forge.domain_catalog import DomainCatalog, default_catalog
 from cstar_forge.forge import settings, source_data
