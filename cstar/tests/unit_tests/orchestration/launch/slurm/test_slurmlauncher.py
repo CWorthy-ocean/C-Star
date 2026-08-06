7b9a74ef051fba0a825482ee064cadd1a33559eb
Chris McBride <3595025+ankona@users.noreply.github.com>
2026-08-05 21:19:40 -0400

    add slurmlauncher unit tests

--- cstar/tests/unit_tests/orchestration/launch/slurm/test_slurmlauncher.py
+++ cstar/tests/unit_tests/orchestration/launch/slurm/test_slurmlauncher.py
@@ -1,129 +1,148 @@
+import os
+import typing as t
 from pathlib import Path
 from unittest import mock
 
 import pytest
 
-from cstar.orchestration.launch.local import LocalComputeSpec, LocalLauncher
+from cstar.orchestration.launch.slurm import SlurmLauncher
 from cstar.orchestration.orchestration import LiveStep, Workplan
 from cstar.orchestration.serialization import deserialize
+from cstar.orchestration.utils import (
+    ENV_CSTAR_SLURM_ACCOUNT,
+    ENV_CSTAR_SLURM_MAX_WALLTIME,
+    ENV_CSTAR_SLURM_QUEUE,
+)
+from cstar.system.scheduler import SlurmPartition, SlurmScheduler
 
 
+def fake_get_queue(name: str = "fake-queue") -> t.Any:
+    return SlurmPartition(name, "cannot-query", lambda x: "48:00:00")
+
+
+@pytest.mark.usefixtures("read_yaml_intercept")
+def test_proxiedrunrequestformatter_wp_intercept(wp_templates_dir: Path) -> None:
+    """Verify that a formatter is initialized correctly when only a step is supplied."""
+    wp_path = wp_templates_dir / "single_step.yaml"
+    workplan = deserialize(wp_path, Workplan)
+    live_step = LiveStep.from_step(workplan.steps[0])
+
+    assert live_step
+
+
+@pytest.mark.usefixtures("read_yaml_intercept")
+def test_slurmlauncher_adapt_step_no_overrides(
+    tmp_path: Path,
+    wp_templates_dir: Path,
+) -> None:
+    """Verify that the `SlurmLauncher` correctly converts a step into a `SchedulerJob`."""
+    wp_path = wp_templates_dir / "single_step.yaml"
+    workplan = deserialize(wp_path, Workplan)
+    live_step = LiveStep.from_step(
+        workplan.steps[0],
+    )
+    minimum_spec = SlurmLauncher._get_default_compute_spec(live_step)  # type: ignore
+
+    mock_mgr = mock.Mock()
+    mock_mgr.environment.package_root = tmp_path
+    mock_mgr.scheduler = SlurmScheduler(
+        queues=[fake_get_queue("a"), fake_get_queue("b")],
+        primary_queue_name="a",
+        other_scheduler_directives={},
+        requires_task_distribution=False,
+        documentation="fake slurm scheduduler",
+        max_cpus_per_node=128,
+    )
+    mock_getsysmgr = mock.Mock(return_value=mock_mgr)
+
+    with (
+        mock.patch("cstar.execution.scheduler_job.get_sysmgr", mock_getsysmgr),
+        mock.patch.object(mock_mgr.scheduler, "get_queue", fake_get_queue),
+    ):
+        job = SlurmLauncher.adapt_step(live_step, [])
+
+    # confirm that no job attributes have been overridden from the minimal spec
+    assert minimum_spec.account_name == job.account_key
+    assert minimum_spec.num_cpus == job.cpus
+    assert minimum_spec.queue_name == job.queue_name
+    assert minimum_spec.max_walltime == job.walltime
+
+    # confirm default behaviors of `create_scheduler_job` for unspecified spec attributes
+    assert minimum_spec.num_nodes == job.nodes
+    assert minimum_spec.cpus_per_node == job.cpus_per_node
+
+
+@pytest.mark.usefixtures("read_yaml_intercept")
 @pytest.mark.parametrize(
-    ("use_proxy", "overrides"),
+    ("overrides",),
     [
-        pytest.param(True, {"local": {}}, id="Proxied; empty local overrides"),
-        pytest.param(True, {}, id="Proxied; no overrides"),
-        pytest.param(False, {"local": {}}, id="Unproxied; empty local overrides"),
-        pytest.param(False, {}, id="Proxied; no overrides"),
+        pytest.param({"slurm": {"queue_name": "b"}}, id="queue-name"),
+        pytest.param({"slurm": {"num_cpus": "42"}}, id="num-cpus"),
+        pytest.param({"slurm": {"num_nodes": "99"}}, id="num-nodes"),
+        pytest.param({"slurm": {"max_walltime": "01:00:00"}}, id="walltime"),
+        pytest.param({"slurm": {"cpus_per_node": "32"}}, id="cpus-per-node"),
+        pytest.param({"slurm": {"account_name": "alt-acct"}}, id="account-name"),
     ],
 )
-def test_locallauncher_adapt_step_formatter_selection(
+def test_slurmlauncher_adapt_step_with_overrides(
+    tmp_path: Path,
     wp_templates_dir: Path,
-    use_proxy: bool,
     overrides: dict[str, dict[str, str]],
 ) -> None:
-    """Verify that the `LocalLauncher` correctly converts a step into a proxied (or
-    non-proxied) command script based on the value of `LocalLauncher.use_proxy`.
-
-    The compute overrides are parameterized to ensure that empty overrides and not
-    specifying overrides result in the same behavior.
-
-    TODO: consider evaluating an empty overrides block as a configuration failure, instead.
+    """Verify that the `SlurmLauncher` correctly converts a step into a `SchedulerJob`
+    that includes any provided per-step compute overrides.
     """
     wp_path = wp_templates_dir / "single_step.yaml"
     workplan = deserialize(wp_path, Workplan)
     live_step = LiveStep.from_step(
         workplan.steps[0],
-        update={
-            "compute_overrides": overrides,
-        },
+        update={"compute_overrides": overrides},
     )
 
-    with mock.patch.object(
-        LocalLauncher, "use_proxy", mock.PropertyMock(return_value=use_proxy)
-    ):
-        step_command = LocalLauncher.adapt_step(live_step, [])
+    mock_mgr = mock.Mock()
+    mock_mgr.environment.package_root = tmp_path
+    mock_mgr.scheduler = SlurmScheduler(
+        queues=[fake_get_queue("a"), fake_get_queue("b")],
+        primary_queue_name="a",
+        other_scheduler_directives={},
+        requires_task_distribution=False,
+        documentation="fake slurm scheduduler",
+        max_cpus_per_node=128,
+    )
+    mock_getsysmgr = mock.Mock(return_value=mock_mgr)
 
-    if use_proxy:
-        assert "update_status" in step_command
+    with (
+        mock.patch.dict(
+            os.environ,
+            {
+                ENV_CSTAR_SLURM_ACCOUNT: "og-account",
+                ENV_CSTAR_SLURM_MAX_WALLTIME: "00:42:00",
+                ENV_CSTAR_SLURM_QUEUE: "a",
+            },
+        ),
+        mock.patch("cstar.execution.scheduler_job.get_sysmgr", mock_getsysmgr),
+        mock.patch.object(mock_mgr.scheduler, "get_queue", fake_get_queue),
+    ):
+        minimum_spec = SlurmLauncher._get_default_compute_spec(live_step)  # type: ignore
+        job = SlurmLauncher.adapt_step(live_step, [])
+
+    # confirm that no job attributes have been overridden from the minimal spec
+    if value := overrides["slurm"].get("account_name", ""):
+        assert job.account_key == value
     else:
-        # confirm the command does not contain proxy-specific script elements
-        assert "update_status" not in step_command
+        assert job.account_key == minimum_spec.account_name
 
-    # confirm that compute overrides are required to modify the command
-    assert "timeout" not in step_command
+    if value := overrides["slurm"].get("num_cpus", ""):
+        assert job.cpus == int(value)
+    else:
+        assert job.cpus == minimum_spec.num_cpus
 
+    if value := overrides["slurm"].get("queue_name", ""):
+        assert job.queue_name == value
+    else:
+        assert job.queue_name == minimum_spec.queue_name
 
-@pytest.mark.parametrize(
-    ("use_proxy", "overrides", "exp_timeout", "exp_fk_timeout"),
-    [
-        pytest.param(
-            True,
-            {"local": {"max_walltime": "90s"}},
-            "90s",
-            LocalComputeSpec.DEFAULT_FK_TIMEOUT,
-            id="Proxied; default fk-to",
-        ),
-        pytest.param(
-            True,
-            {"local": {"force_kill_timeout": "5s"}},
-            LocalComputeSpec.DEFAULT_MAX_WALLTIME,
-            "5s",
-            id="Proxied; default to",
-        ),
-        pytest.param(
-            True,
-            {"local": {"max_walltime": "90s", "force_kill_timeout": "5s"}},
-            "90s",
-            "5s",
-            id="Proxied; no defaults",
-        ),
-        pytest.param(
-            False,
-            {"local": {"max_walltime": "90s"}},
-            "90s",
-            LocalComputeSpec.DEFAULT_FK_TIMEOUT,
-            id="Unproxied; default fk-to",
-        ),
-        pytest.param(
-            False,
-            {"local": {"force_kill_timeout": "5s"}},
-            LocalComputeSpec.DEFAULT_MAX_WALLTIME,
-            "5s",
-            id="Unproxied; default to",
-        ),
-        pytest.param(
-            False,
-            {"local": {"max_walltime": "90s", "force_kill_timeout": "5s"}},
-            "90s",
-            "5s",
-            id="Unproxied; no defaults",
-        ),
-    ],
-)
-def test_locallauncher_adapt_step_with_compute_overrides(
-    wp_templates_dir: Path,
-    use_proxy: bool,
-    overrides: dict[str, dict[str, str]],
-    exp_timeout: str,
-    exp_fk_timeout: str,
-) -> None:
-    """Verify that the `LocalLauncher.adapt` results in the appropriate change
-    to the underlying command and user-specified overrides are honored.
-    """
-    wp_path = wp_templates_dir / "single_step.yaml"
-    workplan = deserialize(wp_path, Workplan)
-    live_step = LiveStep.from_step(
-        workplan.steps[0],
-        update={
-            "compute_overrides": overrides,
-        },
-    )
-
-    with mock.patch.object(
-        LocalLauncher, "use_proxy", mock.PropertyMock(return_value=use_proxy)
-    ):
-        step_command = LocalLauncher.adapt_step(live_step, [])
-
-    # confirm that compute overrides are required to modify the command
-    assert f"timeout {exp_timeout} -k {exp_fk_timeout}" in step_command
+    if value := overrides["slurm"].get("max_walltime", ""):
+        assert job.walltime == value
+    else:
+        assert job.walltime == minimum_spec.max_walltime
